7394fefb54971604fbff565c8e106433503adf7a
Plagman <Plagman@1a8010ca-5511-0410-912e-c29ae57300e0>
2006-08-29 02:02:28 +0000

    Forgot to add the header.
    git-svn-id: https://svn.eduke32.com/eduke32@279 1a8010ca-5511-0410-912e-c29ae57300e0

---
+++ polymer/build/include/polymer.h
@@ -0,0 +1,18 @@
+// blah
+
+#ifndef _polymer_h_
+#define _polymer_h_
+
+// geometry container
+typedef struct  DNF
+{
+    double      v1[3], v2[3], v3[3];
+    double      u1[2], u2[2], u3[2];
+    short       picnum;
+}               _poly;
+
+void    polymer_glinit(void);
+void    polymer_init(void);
+void    polymer_drawrooms(long daposx, long daposy, long daposz, short daang, long dahoriz, short dacursectnum);
+
+#endif // !_polymer_h_
