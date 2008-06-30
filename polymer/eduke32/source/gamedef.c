71292bb5f131753b38bf64e53dc02ec1ffa36d6f
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2008-06-30 18:33:04 +0000

    git-svn-id: https://svn.eduke32.com/eduke32@825 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/source/gamedef.c
+++ polymer/eduke32/source/gamedef.c
@@ -4289,7 +4289,7 @@ repeatcase:
             gamefunctions[j][i] = *textptr;
             keydefaults[j*3][i] = *textptr;
             textptr++,i++;
-            if (*textptr == '/')
+            if (*textptr == '/' || *textptr == ' ')
             {
                 initprintf("%s:%d: warning: invalid character in function name.\n",compilefile,line_number);
                 while (*textptr != 0x0a && *textptr != 0x0d && *textptr != 0) textptr++;
@@ -4297,8 +4297,8 @@ repeatcase:
             }
             if (i >= MAXGAMEFUNCLEN-1)
             {
-                initprintf("%s:%d: error: function name exceeds limit of %d characters.\n",compilefile,line_number,MAXGAMEFUNCLEN);
-                error++;
+                initprintf("%s:%d: warning: function name exceeds limit of %d characters.\n",compilefile,line_number,MAXGAMEFUNCLEN);
+//                error++;
                 while (*textptr != 0x0a && *textptr != 0x0d && *textptr != 0) textptr++;
                 break;
             }
