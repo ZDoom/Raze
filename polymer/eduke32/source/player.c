b8a3c94014c4130caea0df61950a0eedafd2d552
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2008-08-03 12:14:30 +0000

    fix sync error
    git-svn-id: https://svn.eduke32.com/eduke32@930 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/source/player.c
+++ polymer/eduke32/source/player.c
@@ -3604,7 +3604,7 @@ void processinput(int snum)
                         if (ud.config.ScreenWidth >= 800)
                             adduserquote(tempbuf);
                         else OSD_Printf("%s\n",tempbuf);
-                    }
+                    } else krand();
                 }
                 else
                 {
