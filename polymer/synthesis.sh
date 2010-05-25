ff325e2205d53050d514e906bbc8c4b96f73813a
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2010-05-25 11:26:09 +0000

    probably fix snapshot filenames
    git-svn-id: https://svn.eduke32.com/eduke32@1654 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/synthesis.sh
+++ polymer/synthesis.sh
@@ -88,29 +88,29 @@ then
     mkdir $output/$date-$head
     
     # package the binary snapshot
-    echo zip -9 $output/$date-$head/$basename_$platform_$date-$head.zip ${bin_packaged[@]}
-    zip -9 $output/$date-$head/$basename_$platform_$date-$head.zip ${bin_packaged[@]}
+    echo zip -9 $output/$date-$head/${basename}_${platform}_$date-$head.zip ${bin_packaged[@]}
+    zip -9 $output/$date-$head/${basename}_${platform}_$date-$head.zip ${bin_packaged[@]}
     
     # hack to restore [e]obj/keep.me
     echo svn update -r $head
     svn update -r $head
     
     # export the source tree into the output directory
-    svn export . $output/$date-$head/$basename_$date-$head
-    echo svn export . $output/$date-$head/$basename_$date-$head
+    svn export . $output/$date-$head/${basename}_$date-$head
+    echo svn export . $output/$date-$head/${basename}_$date-$head
     
     # package the source
     cd $output/$date-$head
     
     # first remove the unnecessary files
     for i in "${not_src_packaged[@]}"; do
-        echo rm -r $basename_$date-$head/$i
-        rm -r $basename_$date-$head/$i
+        echo rm -r ${basename}_$date-$head/$i
+        rm -r ${basename}_$date-$head/$i
     done
     
-    echo tar cjf $basename_src_$date-$head.tar.bz2 $basename_$date-$head
-    tar cjf $basename_src_$date-$head.tar.bz2 $basename_$date-$head
-    rm -r $basename_$date-$head
+    echo tar cjf ${basename}_src_$date-$head.tar.bz2 ${basename}_$date-$head
+    tar cjf ${basename}_src_$date-$head.tar.bz2 ${basename}_$date-$head
+    rm -r ${basename}_$date-$head
     
     # output the changelog since last snapshot in the output directory
     if [  $lastrevision ]
