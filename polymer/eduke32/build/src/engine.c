1eec50be5fabdae35689c9d499145a7fbb3fdd66
helixhorned <helixhorned@1a8010ca-5511-0410-912e-c29ae57300e0>
2011-06-27 10:55:08 +0000

    make min. resolution 320x200 (Hendricks266's request)
    git-svn-id: https://svn.eduke32.com/eduke32@1923 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/build/src/engine.c
+++ polymer/eduke32/build/src/engine.c
@@ -9740,8 +9740,8 @@ int32_t setgamemode(char davidoption, int32_t daxdim, int32_t daydim, int32_t da
 {
     int32_t i, j;
 
-    daxdim = max(640, daxdim);
-    daydim = max(480, daydim);
+    daxdim = max(320, daxdim);
+    daydim = max(200, daydim);
 
 #ifdef USE_OPENGL
     extern char nogl;
