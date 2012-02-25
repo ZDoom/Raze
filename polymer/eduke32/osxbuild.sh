f524f8f98c651ee6a09c57130e90b53c1e10285d
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2012-02-25 03:31:20 +0000

    Update "osxbuild.sh" to reflect implementation of .app bundles on Mac OS X.
    git-svn-id: https://svn.eduke32.com/eduke32@2386 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/osxbuild.sh
+++ polymer/eduke32/osxbuild.sh
@@ -25,8 +25,8 @@ if [ $onlyzip -eq 0 ]; then
     # WITHOUT_GTK=1 RELEASE=0 BUILD32_ON_64=0 make -j 3
     # if [ $? ]; then
     #     echo 64-bit debug build succeeded.
-    #     cp mapster32 mapster32.debug.x64
-    #     cp eduke32 eduke32.debug.x64
+    #     cp "Mapster32.app/Contents/MacOS/mapster32" mapster32.debug.x64
+    #     cp "EDuke32.app/Contents/MacOS/eduke32" eduke32.debug.x64
     # else
     #     echo 64-bit debug build failed.
     # fi
@@ -35,8 +35,8 @@ if [ $onlyzip -eq 0 ]; then
     WITHOUT_GTK=1 RELEASE=1 BUILD32_ON_64=0 USE_LIBVPX=1 make -j 3
     if [ $? ]; then
         echo 64-bit release build succeeded.
-        cp mapster32 mapster32.x64
-        cp eduke32 eduke32.x64
+        cp "Mapster32.app/Contents/MacOS/mapster32" mapster32.x64
+        cp "EDuke32.app/Contents/MacOS/eduke32" eduke32.x64
     else
         echo 64-bit release build failed.
     fi
@@ -45,8 +45,8 @@ if [ $onlyzip -eq 0 ]; then
     # WITHOUT_GTK=1 RELEASE=0 BUILD32_ON_64=1 make -j 3
     # if [ $? ]; then
     #     echo 32-bit debug build succeeded.
-    #     cp mapster32 mapster32.debug.x86
-    #     cp eduke32 eduke32.debug.x86
+    #     cp "Mapster32.app/Contents/MacOS/mapster32" mapster32.debug.x86
+    #     cp "EDuke32.app/Contents/MacOS/eduke32" eduke32.debug.x86
     # else
     #     echo 32-bit debug build failed.
     # fi
@@ -55,8 +55,8 @@ if [ $onlyzip -eq 0 ]; then
     WITHOUT_GTK=1 RELEASE=1 BUILD32_ON_64=1 USE_LIBVPX=0 make -j 3
     if [ $? ]; then
         echo 32-bit release build succeeded.
-        cp mapster32 mapster32.x86
-        cp eduke32 eduke32.x86
+        cp "Mapster32.app/Contents/MacOS/mapster32" mapster32.x86
+        cp "EDuke32.app/Contents/MacOS/eduke32" eduke32.x86
     else
         echo 32-bit release build failed.
     fi
@@ -65,8 +65,8 @@ if [ $onlyzip -eq 0 ]; then
     # ARCH='-arch ppc' WITHOUT_GTK=1 RELEASE=0 BUILD32_ON_64=0 make -j 3
     # if [ $? ]; then
     #     echo PowerPC debug build succeeded.
-    #     cp mapster32 mapster32.debug.ppc
-    #     cp eduke32 eduke32.debug.ppc
+    #     cp "Mapster32.app/Contents/MacOS/mapster32" mapster32.debug.ppc
+    #     cp "EDuke32.app/Contents/MacOS/eduke32" eduke32.debug.ppc
     # else
     #     echo PowerPC debug build failed.
     # fi
@@ -75,20 +75,37 @@ if [ $onlyzip -eq 0 ]; then
     # ARCH='-arch ppc' WITHOUT_GTK=1 RELEASE=1 BUILD32_ON_64=0 USE_LIBVPX=0 make -j 3
     # if [ $? ]; then
     #     echo PowerPC release build succeeded.
-    #     cp mapster32 mapster32.ppc
-    #     cp eduke32 eduke32.ppc
+    #     cp "Mapster32.app/Contents/MacOS/mapster32" mapster32.ppc
+    #     cp "EDuke32.app/Contents/MacOS/eduke32" eduke32.ppc
     # else
     #     echo PowerPC release build failed.
     # fi
 fi
 
+# Duplicating .app bundles for debug build:
+# if [ -d "Mapster32.app" ]; then
+    # cp "Mapster32.app" "Mapster32.debug.app"
+# fi
+# if [ -d "EDuke32.app" ]; then
+    # cp "EDuke32.app" "EDuke32.debug.app"
+# fi
+
 # Almost done...
 if [ -f mapster32.x64 ] && [ -f eduke32.x86 ] && [ -f eduke32.ppc ]; then
     echo Creating fat binaries.
+
     lipo -create mapster32.x64 mapster32.x86  -output mapster32
+    cp -f mapster32 "Mapster32.app/Contents/MacOS/mapster32"
+
 #    lipo -create mapster32.debug.x64 mapster32.debug.x86 -output mapster32.debug
+#    cp -f mapster32 "Mapster32.debug.app/Contents/MacOS/mapster32"
+
     lipo -create eduke32.x64 eduke32.x86  -output eduke32
+    cp -f eduke32 "EDuke32.app/Contents/MacOS/eduke32"
+
 #    lipo -create eduke32.debug.x64 eduke32.debug.x86 -output eduke32.debug
+#    cp -f eduke32 "EDuke32.debug.app/Contents/MacOS/eduke32"
+
     if [ -d ".svn" ]; then
         rev=`svn info | grep Revision | awk '{ print $2 }'`
     else
@@ -101,5 +118,6 @@ if [ -f mapster32.x64 ] && [ -f eduke32.x86 ] && [ -f eduke32.ppc ]; then
     echo "The 64-bit build in this archive has LibVPX (http://www.webmproject.org/code/)" >> README.OSX
     echo "from MacPorts (http://www.macports.org/) statically linked into it." >> README.OSX
     rm -f "$arfilename"
-    zip "$arfilename" mapster32 eduke32 README.OSX
+    zip "$arfilename" Mapster32.app EDuke32.app README.OSX
+    # Mapster32.debug.app EDuke32.debug.app
 fi
