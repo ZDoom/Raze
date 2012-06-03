2df94e8bc7c40de0bf6c46eba3a00dbb9241b74b
helixhorned <helixhorned@1a8010ca-5511-0410-912e-c29ae57300e0>
2012-06-03 19:20:32 +0000

    Fix a "set but not used warning" in xdelta3.c, revert definition of XD3_ASSERT.
    git-svn-id: https://svn.eduke32.com/eduke32@2733 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/source/xdelta3/xdelta3.h
+++ polymer/eduke32/source/xdelta3/xdelta3.h
@@ -297,7 +297,7 @@ typedef int              (xd3_comp_table_func) (xd3_stream *stream,
     do { if (! (x)) { DP(RINT "%s:%d: XD3 assertion failed: %s\n", __FILE__, __LINE__, #x); \
     abort (); } } while (0)
 #else
-#define XD3_ASSERT(x)
+#define XD3_ASSERT(x) (void)0
 #endif  /* XD3_DEBUG */
 
 #ifdef __GNUC__
