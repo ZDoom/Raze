afd0005c2842045b956f30cd76892e1f6d82b5c9
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2013-01-27 21:37:21 +0000

    Fix MinGW/GCC build. Oops!
    git-svn-id: https://svn.eduke32.com/eduke32@3442 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/source/osdcmds.c
+++ polymer/eduke32/source/osdcmds.c
@@ -36,6 +36,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <ctype.h>
 #include <limits.h>
+#include <math.h>
 #include "enet/enet.h"
 
 extern int32_t voting, g_doQuickSave;
