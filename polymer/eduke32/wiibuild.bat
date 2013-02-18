90f4a02905d3f8e6c690879e6b4158f9fc76f39d
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2013-02-18 08:49:20 +0000

    Wii: Add Homebrew Channel metadata for Mapster32 and expand the build process to easily allow its addition. Unfortunately it closes on launch without any log and I don't have any means to debug it, so it remains disabled. (Who wants to map on a TV with 640x480 resolution anyway?)
    git-svn-id: https://svn.eduke32.com/eduke32@3495 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/wiibuild.bat
+++ polymer/eduke32/wiibuild.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal ENABLEEXTENSIONS DISABLEDELAYEDEXPANSION
 
+set targets=eduke32
 set PATH=C:\devkitPro\devkitPPC\bin;C:\devkitPro\msys\bin;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
 
 :: Detect versioning systems and pull the revision number:
@@ -21,19 +22,23 @@ set buildparameters=PLATFORM=WII %*
 make veryclean %buildparameters%
 make OPTLEVEL=2 LTO=0 %buildparameters%
 
-if not exist "eduke32.elf" goto end
+for %%G in (%targets%) do if not exist "%%~G.elf" goto end
 
 :: Package data:
-xcopy /e /q /y Wii\apps apps\
-for %%G in (eduke32) do for %%H in (.elf) do if exist "%%~G%%~H" move /y "%%~G%%~H" "apps\%%~G\boot%%~H"
-for %%G in (eduke32) do for %%H in (.elf.map) do if exist "%%~G%%~H" del /f /q "%%~G%%~H"
-"echo.exe" -e "    <version>%rev%</version>\n    <release_date>%currentdate%</release_date>" | "cat.exe" Wii\meta_1.xml - Wii\meta_2.xml >"apps\eduke32\meta.xml"
+if not exist apps mkdir apps
+for %%G in (%targets%) do xcopy /e /q /y Wii\apps\%%~G apps\%%~G\
+for %%G in (%targets%) do for %%H in (.elf) do if exist "%%~G%%~H" move /y "%%~G%%~H" "apps\%%~G\boot%%~H"
+for %%G in (%targets%) do for %%H in (.elf.map) do if exist "%%~G%%~H" del /f /q "%%~G%%~H"
+for %%G in (%targets%) do "echo.exe" -e "    <version>%rev%</version>\n    <release_date>%currentdate%</release_date>" | "cat.exe" "Wii\%%~G_meta_1.xml" - "Wii\%%~G_meta_2.xml" >"apps\%%~G\meta.xml"
+
 :: We don't want to package all the stuff, most of it is for Mapster32 and dev materials.
-:: xcopy /e /q /y /EXCLUDE:.dll package apps\eduke32\
 :: However, do throw in the licenses:
 if exist "package\*.txt" copy /y "package\*.txt" "apps\eduke32\"
+
+:: xcopy /e /q /y /EXCLUDE:Wii\xcopy_exclude.txt package apps\mapster32\
+
 "ls.exe" -l -R apps
-7z.exe a -mx9 -t7z eduke32-wii-r%rev%.7z apps -xr!*.svn*
+7z.exe a -mx9 -t7z eduke32-wii-%rev%.7z apps -xr!*.svn*
 
 :end
 
@@ -42,3 +47,4 @@ if "%vc%"=="svn" svn revert source\rev.h
 if "%vc%"=="git" git checkout source\rev.h
 
 endlocal
+goto :eof
