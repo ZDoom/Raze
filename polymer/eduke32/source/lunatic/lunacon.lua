9d3b96a636768d546cb3f3ddcfd34b2bf60f1eb3
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2013-03-25 19:19:05 +0000

    LunaCON: Properly implement rotatesprite16 by adding ROTATESPRITE_FULL16 to orientation.
    git-svn-id: https://svn.eduke32.com/eduke32@3611 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/source/lunatic/lunacon.lua
+++ polymer/eduke32/source/lunatic/lunacon.lua
@@ -1728,8 +1728,8 @@ local handle =
         return format("_con.rotatesprite(%s,%s,%s,%s,%s,%s,%s,%s,0,%s,%s,%s,%s)", ...)
     end,
 
-    rotatesprite16 = function(...)  -- TODO: non <<16 coordinates
-        return format("_con.rotatesprite(%s/65536,%s/65536,%s,%s,%s,%s,%s,%s,0,%s,%s,%s,%s)", ...)
+    rotatesprite16 = function(...)  -- (orientation|ROTATESPRITE_FULL16)
+        return format("_con.rotatesprite(%s,%s,%s,%s,%s,%s,%s,_bit.bor(%s,2048),0,%s,%s,%s,%s)", ...)
     end,
 
     rotatespritea = function(...)
