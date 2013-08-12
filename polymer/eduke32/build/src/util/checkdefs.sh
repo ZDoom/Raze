41793193f5703820cd7763d57814b7accad5a293
helixhorned <helixhorned@1a8010ca-5511-0410-912e-c29ae57300e0>
2013-08-12 15:18:11 +0000

    checkdefs.sh: check first and second args, handle definelevelname.
    git-svn-id: https://svn.eduke32.com/eduke32@4009 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/build/src/util/checkdefs.sh
+++ polymer/eduke32/build/src/util/checkdefs.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
+function usage ()
+{
     echo 'Usage: checkdefs.sh <some.def> [[<search_path_base_dir>] {-patch,-con,-conpatch}]'
     exit 1
+}
+
+if [ -z "$1" ]; then
+    usage
 fi
 deffn="$1"
+
+if [ ! -f "$deffn" ]; then
+    echo "Error: First argument must be a name of an existing DEF or CON file."
+    usage
+fi
 
 if [ -z "$2" ]; then
     thedir=.
 else
     thedir="$2"
+fi
+
+if [ ! -d "$thedir" ]; then
+    echo "Error: Second argument must be a name of an existing directory."
+    usage
 fi
 
 dopatch=""
@@ -34,7 +49,10 @@ if [ -z $docon ]; then
     files=$(grep -E "\".*\..*\"" "$deffn" | sed 's/.*"\(.*\..*\)".*/\1/g')
 else
     # con... this is awful
-    files=$(grep -E -i '(include *.*\.con)|(definesound.*(voc|wav|ogg))' "$deffn" | sed -r 's/.*include[ \t]+([^ \t]+\.[cC][oO][nN]).*/\1/g; s/.*[ \t]([^ \t].+\.([wW][aA][vV]|[vV][oO][cC]|[oO][gG][gG])).*/\1/g')
+    files=$(grep -E -i '(include *.*\.con)|(definesound.*(voc|wav|ogg))|(definelevelname.*\.map)' "$deffn" |
+        sed -r 's/.*include[ \t]+([^ \t]+\.[cC][oO][nN]).*/\1/g;
+                s/.*[ \t]([^ \t].+\.([wW][aA][vV]|[vV][oO][cC]|[oO][gG][gG])).*/\1/g;
+                s/.*[ \t]([^ \t].+\.([mM][aA][pP])).*/\1/g;')
 fi
 
 exfiles=$(find "$thedir" -type f)
