fddc700cd6af733ba15204a945e1f53f621245c5
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2013-10-13 09:16:25 +0000

    Wii: Fix wiibuild.bat so that revision numbers have the "r" prefix like they are supposed to.
    git-svn-id: https://svn.eduke32.com/eduke32@4103 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/platform/wiibuild.bat
+++ polymer/eduke32/platform/wiibuild.bat
@@ -8,13 +8,13 @@ pushd "%~dp0.."
 set wiidir=platform\Wii
 
 :: Detect versioning systems and pull the revision number:
-for /f "delims=" %%G in ('svn info 2^>^&1 ^| grep Revision ^| cut -d " " -f 2') do @set rev=r%%G
+for /f "delims=" %%G in ('svn info 2^>^&1 ^| grep Revision ^| cut -d " " -f 2') do @set rev=%%G
 if not "%rev%"=="" set vc=svn
-if "%rev%"=="" for /f "delims=" %%G in ('git svn info 2^>^&1 ^| grep Revision ^| cut -d " " -f 2') do @set rev=r%%G
+if "%rev%"=="" for /f "delims=" %%G in ('git svn info 2^>^&1 ^| grep Revision ^| cut -d " " -f 2') do @set rev=%%G
 if not "%rev%"=="" set vc=git
 if "%rev%"=="" set vc=none
-if not "%rev%"=="" echo s_buildRev = "%rev%";>source\rev.h
-if "%rev%"=="" set rev=rXXXX
+if not "%rev%"=="" echo s_buildRev = "r%rev%";>source\rev.h
+if "%rev%"=="" set rev=XXXX
 
 :: Get the current date:
 for /f "delims=" %%G in ('"C:\MinGW\msys\1.0\bin\date.exe" +%%Y%%m%%d') do @set currentdate=%%G
@@ -32,7 +32,7 @@ if not exist apps mkdir apps
 for %%G in (%targets%) do xcopy /e /q /y %wiidir%\apps\%%~G apps\%%~G\
 for %%G in (%targets%) do for %%H in (.elf) do if exist "%%~G%%~H" move /y "%%~G%%~H" "apps\%%~G\boot%%~H"
 for %%G in (%targets%) do for %%H in (.elf.map) do if exist "%%~G%%~H" del /f /q "%%~G%%~H"
-for %%G in (%targets%) do "echo.exe" -e "    <version>%rev%</version>\n    <release_date>%currentdate%</release_date>" | "cat.exe" "%wiidir%\%%~G_meta_1.xml" - "%wiidir%\%%~G_meta_2.xml" >"apps\%%~G\meta.xml"
+for %%G in (%targets%) do "echo.exe" -e "    <version>r%rev%</version>\n    <release_date>%currentdate%</release_date>" | "cat.exe" "%wiidir%\%%~G_meta_1.xml" - "%wiidir%\%%~G_meta_2.xml" >"apps\%%~G\meta.xml"
 
 :: We don't want to package all the stuff, most of it is for Mapster32 and dev materials.
 :: However, do throw in the licenses:
@@ -41,7 +41,7 @@ if exist "package\*.txt" copy /y "package\*.txt" "apps\eduke32\"
 xcopy /e /q /y /EXCLUDE:%wiidir%\xcopy_exclude.txt package apps\mapster32\
 
 "ls.exe" -l -R apps
-7z.exe a -mx9 -t7z eduke32-wii-%rev%.7z apps -xr!*.svn*
+7z.exe a -mx9 -t7z eduke32-wii-r%rev%.7z apps -xr!*.svn*
 
 :end
 
