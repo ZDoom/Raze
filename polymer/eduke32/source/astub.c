8fa00afb7cb347170cea810b4525b5ba8b8a8c22
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2014-01-12 14:03:00 +0000

    Mapster32: Add EVENT_LOADMAP, EVENT_SAVEMAP, EVENT_PRELOADMAP, and EVENT_PRESAVEMAP.
    Some are more useful than others.
    
    git-svn-id: https://svn.eduke32.com/eduke32@4244 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/source/astub.c
+++ polymer/eduke32/source/astub.c
@@ -731,12 +731,16 @@ void ExtLoadMap(const char *mapname)
 #if M32_UNDO
     map_undoredo_free();
 #endif
+
+    VM_OnEvent(EVENT_LOADMAP, -1);
 }
 
 void ExtSaveMap(const char *mapname)
 {
     UNREFERENCED_PARAMETER(mapname);
     saveboard("backup.map", &pos, ang, cursectnum);
+
+    VM_OnEvent(EVENT_SAVEMAP, -1);
 }
 
 
@@ -8393,6 +8397,8 @@ int32_t ExtPreSaveMap(void)
 {
     int32_t numfixedsprites;
 
+    VM_OnEvent(EVENT_PRESAVEMAP, -1);
+
     numfixedsprites = fixspritesectors();   //Do this before saving!
     updatesectorz(startpos.x,startpos.y,startpos.z,&startsectnum);
     if (startsectnum < 0)
@@ -10281,6 +10287,7 @@ static int32_t loadconsounds(const char *fn)
 
 void ExtPreLoadMap(void)
 {
+    VM_OnEvent(EVENT_PRELOADMAP, -1);
 }
 
 /// ^^^
