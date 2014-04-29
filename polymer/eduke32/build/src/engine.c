fc77a8aa3dd85508c56b2bed10ddee6c137f41d6
helixhorned <helixhorned@1a8010ca-5511-0410-912e-c29ae57300e0>
2014-04-29 19:34:25 +0000

    engine.c: after creating gray fog palookup, make its greatest shade map to same color.
    Concretely, for all 1 <= i <= 255, make graypalookup[31][i] := graypalookup[31][0].
    Also document 'fogpal' in the Lunatic doc.
    
    git-svn-id: https://svn.eduke32.com/eduke32@4456 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/build/src/engine.c
+++ polymer/eduke32/build/src/engine.c
@@ -8254,10 +8254,15 @@ int32_t loadlookups(int32_t fp, uint8_t **basepaltabptr)
     for (j=1; j<=255-3; j++)
         if (!palookup[j] && !palookup[j+1] && !palookup[j+2] && !palookup[j+3])
         {
+            int32_t i;
+
             makepalookup(j, NULL, 15, 15, 15, 1);
             makepalookup(j+1, NULL, 15, 0, 0, 1);
             makepalookup(j+2, NULL, 0, 15, 0, 1);
             makepalookup(j+3, NULL, 0, 0, 15, 1);
+
+            for (i=1; i<255; i++)
+                palookup[j][((numshades-1)<<8) + i] = palookup[j][((numshades-1)<<8)];
 
             firstfogpal = j;
             break;
