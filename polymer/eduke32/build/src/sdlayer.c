81e9761e50f9c0e1f58a4a1a7bbf4e574727f203
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2014-10-01 17:09:24 +0000

    Set SDL_GL_ACCELERATED_VISUAL to 1
    git-svn-id: https://svn.eduke32.com/eduke32@4641 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/build/src/sdlayer.c
+++ polymer/eduke32/build/src/sdlayer.c
@@ -1534,6 +1534,7 @@ int32_t setvideomode(int32_t x, int32_t y, int32_t c, int32_t fs)
             { SDL_GL_MULTISAMPLEBUFFERS, glmultisample > 0 },
             { SDL_GL_MULTISAMPLESAMPLES, glmultisample },
             { SDL_GL_STENCIL_SIZE, 1 },
+            { SDL_GL_ACCELERATED_VISUAL, 1 },
 # if SDL_MAJOR_VERSION == 1
             { SDL_GL_SWAP_CONTROL, vsync_render },
 # endif
