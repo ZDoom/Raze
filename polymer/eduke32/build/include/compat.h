07bf374269e3f1b2a96bc61307592cdc79335c3d
helixhorned <helixhorned@1a8010ca-5511-0410-912e-c29ae57300e0>
2014-12-01 19:21:48 +0000

    Fix build on platforms not magically #defining UNREFERENCED_PARAMETER. DONT_BUILD.
    git-svn-id: https://svn.eduke32.com/eduke32@4785 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/build/include/compat.h
+++ polymer/eduke32/build/include/compat.h
@@ -26,7 +26,7 @@
 # define __has_extension __has_feature // Compatibility with pre-3.0 compilers.
 #endif
 
-#if defined(_MSC_VER) && _MSC_FULL_VER < 180031101
+#if !defined(_MSC_VER) || _MSC_FULL_VER < 180031101
 #ifdef UNREFERENCED_PARAMETER
 #undef UNREFERENCED_PARAMETER
 #endif
