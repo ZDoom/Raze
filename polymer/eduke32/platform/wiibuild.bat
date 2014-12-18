30407c8e87adb4824207357444ca96539b4ad753
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2014-12-18 18:15:05 +0000

    Wii: Output and package the binary as .dol instead of .elf.
    git-svn-id: https://svn.eduke32.com/eduke32@4824 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/platform/wiibuild.bat
+++ polymer/eduke32/platform/wiibuild.bat
@@ -2,7 +2,7 @@
 setlocal ENABLEEXTENSIONS DISABLEDELAYEDEXPANSION
 
 set targets=eduke32 mapster32
-set PATH=C:\devkitPro\devkitPPC\bin;C:\devkitPro\msys\bin;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
+set PATH=C:\devkitPro\devkitPPC\bin;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;C:\devkitPro\msys\bin;%PATH%
 set DEVKITPPC=C:/devkitPro/devkitPPC
 set DEVKITPRO=C:/devkitPro
 
@@ -25,16 +25,12 @@ set commandline=make PLATFORM=WII %* STRIP=""
 echo %commandline%
 %commandline%
 
-for %%G in (%targets%) do if not exist "%%~G.elf" goto end
-
-:: Strip:
-for %%G in (%targets%) do for %%H in (.elf) do if exist "%%~G%%~H" copy /y "%%~G%%~H" "%%~G-unstripped%%~H"
-for %%G in (%targets%) do for %%H in (.elf) do if exist "%%~G%%~H" powerpc-eabi-strip "%%~G%%~H"
+for %%G in (%targets%) do if not exist "%%~G.dol" goto end
 
 :: Package data:
 if not exist apps mkdir apps
 for %%G in (%targets%) do xcopy /e /q /y %wiidir%\apps\%%~G apps\%%~G\
-for %%G in (%targets%) do for %%H in (.elf) do if exist "%%~G%%~H" move /y "%%~G%%~H" "apps\%%~G\boot%%~H"
+for %%G in (%targets%) do for %%H in (.dol) do if exist "%%~G%%~H" move /y "%%~G%%~H" "apps\%%~G\boot%%~H"
 for %%G in (%targets%) do for %%H in (.elf.map) do if exist "%%~G%%~H" del /f /q "%%~G%%~H"
 for %%G in (%targets%) do "echo.exe" -e "    <version>r%rev%</version>\n    <release_date>%currentdate%</release_date>" | "cat.exe" "%wiidir%\%%~G_meta_1.xml" - "%wiidir%\%%~G_meta_2.xml" >"apps\%%~G\meta.xml"
 
@@ -44,6 +40,7 @@ xcopy /e /q /y /EXCLUDE:%wiidir%\xcopy_exclude.txt package\common apps\mapster32
 xcopy /e /q /y /EXCLUDE:%wiidir%\xcopy_exclude.txt package\sdk apps\mapster32\
 
 "ls.exe" -l -R apps
+7z.exe a -mx9 -t7z eduke32-wii-r%rev%-debug-elf.7z *.elf -xr!*.svn*
 7z.exe a -mx9 -t7z eduke32-wii-r%rev%.7z apps -xr!*.svn*
 
 :end
