e577335bd97d2ca99e070e24ac871ad9d8c1ae48
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2016-03-07 11:22:07 +0000

    Revert r5601 in its entirety. This fixes MIDI playback on Windows.
    git-svn-id: https://svn.eduke32.com/eduke32@5650 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/source/midi.h
+++ polymer/eduke32/source/midi.h
@@ -1,12 +1,10 @@
-//-------------------------------------------------------------------------
 /*
-Copyright (C) 2016 EDuke32 developers and contributors
+Copyright (C) 1994-1995 Apogee Software, Ltd.
 
-This file is part of EDuke32.
-
-EDuke32 is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License version 2
-as published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,22 +15,36 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Modifications for JonoF's port by Jonathon Fowler (jf@jonof.id.au)
 */
-//-------------------------------------------------------------------------
+/**********************************************************************
+   module: MIDI.H
+
+   author: James R. Dose
+   date:   May 25, 1994
+
+   Public header for MIDI.C.  Midi song file playback routines.
+
+   (c) Copyright 1994 James R. Dose.  All Rights Reserved.
+**********************************************************************/
 
 #ifndef __MIDI_H
 #define __MIDI_H
 
 enum MIDI_Errors
-{
-    MIDI_Error = -1,
-    MIDI_Ok = 0,
-    MIDI_NullMidiModule,
-    MIDI_InvalidMidiFile,
-    MIDI_UnknownMidiFormat,
-    MIDI_NoTracks,
-    MIDI_InvalidTrack,
-};
+   {
+   MIDI_Warning = -2,
+   MIDI_Error   = -1,
+   MIDI_Ok      = 0,
+   MIDI_NullMidiModule,
+   MIDI_InvalidMidiFile,
+   MIDI_UnknownMidiFormat,
+   MIDI_NoTracks,
+   MIDI_InvalidTrack,
+   MIDI_NoMemory,
+   MIDI_DPMI_Error
+   };
 
 
 #define MIDI_PASS_THROUGH 1
@@ -43,26 +55,46 @@ enum MIDI_Errors
 extern char MIDI_PatchMap[ 128 ];
 
 typedef struct
-{
-    void(*NoteOff)(char channel, char key, char velocity);
-    void(*NoteOn)(char channel, char key, char velocity);
-    void(*PolyAftertouch)(char channel, char key, char pressure);
-    void(*ControlChange)(char channel, char number, char value);
-    void(*ProgramChange)(char channel, char program);
-    void(*ChannelAftertouch)(char channel, char pressure);
-    void(*PitchBend)(char channel, char lsb, char msb);
-} midifuncs;
+   {
+   void ( *NoteOff )( int32_t channel, int32_t key, int32_t velocity );
+   void ( *NoteOn )( int32_t channel, int32_t key, int32_t velocity );
+   void ( *PolyAftertouch )( int32_t channel, int32_t key, int32_t pressure );
+   void ( *ControlChange )( int32_t channel, int32_t number, int32_t value );
+   void ( *ProgramChange )( int32_t channel, int32_t program );
+   void ( *ChannelAftertouch )( int32_t channel, int32_t pressure );
+   void ( *PitchBend )( int32_t channel, int32_t lsb, int32_t msb );
+   void ( *ReleasePatches )( void );
+   void ( *LoadPatch )( int32_t number );
+   void ( *SetVolume )( int32_t volume );
+   int32_t  ( *GetVolume )( void );
+   void ( *FinishBuffer )( void );
+   } midifuncs;
 
-int32_t     MIDI_AllNotesOff(void);
-int32_t     MIDI_Reset(void);
-int32_t     MIDI_SetVolume(int32_t volume);
-void        MIDI_SetMidiFuncs(midifuncs *funcs);
-void        MIDI_ContinueSong(void);
-void        MIDI_PauseSong(void);
-void        MIDI_StopSong(void);
-int32_t     MIDI_PlaySong(char *song, int32_t loopflag);
-void        MIDI_SetTempo(int32_t tempo);
-void        MIDI_UpdateMusic(void);
-void        MIDI_SetDivision(int32_t division);
+void MIDI_RerouteMidiChannel( int32_t channel, int32_t ( *function )( int32_t event, int32_t c1, int32_t c2 ) );
+int32_t  MIDI_AllNotesOff( void );
+void MIDI_SetUserChannelVolume( int32_t channel, int32_t volume );
+void MIDI_ResetUserChannelVolume( void );
+int32_t  MIDI_Reset( void );
+int32_t  MIDI_SetVolume( int32_t volume );
+int32_t  MIDI_GetVolume( void );
+void MIDI_SetMidiFuncs( midifuncs *funcs );
+void MIDI_SetContext( int32_t context );
+int32_t  MIDI_GetContext( void );
+void MIDI_SetLoopFlag( int32_t loopflag );
+void MIDI_ContinueSong( void );
+void MIDI_PauseSong( void );
+int32_t  MIDI_SongPlaying( void );
+void MIDI_StopSong( void );
+int32_t  MIDI_PlaySong( char *song, int32_t loopflag );
+void MIDI_SetTempo( int32_t tempo );
+int32_t  MIDI_GetTempo( void );
+void MIDI_SetSongTick( uint32_t PositionInTicks );
+void MIDI_SetSongTime( uint32_t milliseconds );
+void MIDI_SetSongPosition( int32_t measure, int32_t beat, int32_t tick );
+void MIDI_GetSongPosition( songposition *pos );
+void MIDI_GetSongLength( songposition *pos );
+void MIDI_LoadTimbres( void );
+void MIDI_UpdateMusic(void);
+void MIDI_SetDivision( int32_t division );
 
 #endif
