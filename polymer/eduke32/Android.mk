e1ee089da5db0da3bc9c21c42dd321027a384be5
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2016-03-28 05:15:23 +0000

    Android: Minor build system cleanup.
    git-svn-id: https://svn.eduke32.com/eduke32@5677 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/Android.mk
+++ polymer/eduke32/Android.mk
@@ -4,23 +4,25 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := duke3d
 
-LOCAL_CFLAGS    := -x c++ -std=gnu++11 -fvisibility=hidden -fPIC -funsigned-char -fno-strict-aliasing -DNO_GCC_BUILTINS -pthread -DHAVE_INTTYPES  -D_GNU_SOURCE=1 -D_REENTRANT
-
-ifeq ($(NDK_DEBUG), 1)
-    LOCAL_CFLAGS += -O0 -ggdb -fno-omit-frame-pointer -fno-stack-protector -D_FORTIFY_SOURCE=0
-else
-    LOCAL_CFLAGS += -O2 -DNDEBUG -DUSING_LTO -flto -D_FORTIFY_SOURCE=2
-endif
-
-LOCAL_CPPFLAGS  := -std=gnu++11
-
-LOCAL_CFLAGS    += -W -Werror-implicit-function-declaration -Wpointer-arith -Wextra -Wno-unused-result -Wno-char-subscripts \
-                    -Wno-strict-overflow -Wno-attributes -Wno-write-strings -mhard-float -D_NDK_MATH_NO_SOFTFP=1
-LOCAL_CFLAGS    += -DHAVE_SDL -DHAVE_VORBIS -DHAVE_JWZGLES -DHAVE_ANDROID -DRENDERTYPESDL=1 -DUSE_OPENGL -DNETCODE_DISABLE -DUSE_LIBVPX
+COMMONFLAGS     := -x c++ -std=gnu++11 -fvisibility=hidden -fPIC -funsigned-char -fno-strict-aliasing -pthread -mhard-float \
+                   -W -Wall -Wextra -Wpointer-arith -Wno-char-subscripts -Wno-missing-braces -Wwrite-strings -Wuninitialized \
+                   -Wno-attributes -Wno-strict-overflow -Wno-unused-result -Wlogical-op -Wcast-qual \
+                   -DHAVE_SDL -DHAVE_VORBIS -DHAVE_JWZGLES -DHAVE_ANDROID -DRENDERTYPESDL=1 -DUSE_OPENGL -DNETCODE_DISABLE -DUSE_LIBVPX \
+                   -DHAVE_INTTYPES -D_GNU_SOURCE=1 -D_REENTRANT -D_NDK_MATH_NO_SOFTFP=1
 
 LOCAL_LDFLAGS   := -fuse-ld=bfd
 TARGET_LDFLAGS  += -Wl,--no-warn-mismatch -lm_hard
 LOCAL_ARM_NEON  = true
+
+ifeq ($(NDK_DEBUG), 1)
+    COMMONFLAGS += -O0 -ggdb -fno-omit-frame-pointer -fno-stack-protector -D_FORTIFY_SOURCE=0 -DDEBUGGINGAIDS=0
+else
+    COMMONFLAGS += -O2 -DNDEBUG -DUSING_LTO -flto -D_FORTIFY_SOURCE=2
+    LOCAL_LDFLAGS += -flto
+endif
+
+LOCAL_CFLAGS    = $(COMMONFLAGS)
+LOCAL_CPPFLAGS  = $(COMMONFLAGS)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/source $(LOCAL_PATH)/source/jmact $(LOCAL_PATH)/source/jaudiolib/include $(LOCAL_PATH)/source/enet/include $(LOCAL_PATH)/build/include
 LOCAL_C_INCLUDES += $(TOP_DIR)/ $(TOP_DIR)/Libraries/liboggvorbis/include $(TOP_DIR)/Libraries/ $(TOP_DIR)/Libraries/SDL2/include $(TOP_DIR)/Libraries/SDL2_mixer/include $(TOP_DIR)/Libraries/TinyXML/include $(TOP_DIR)/TouchControls $(TOP_DIR)/Libraries/libvpx/include
@@ -60,7 +62,8 @@ BUILD_SRC = \
     build/src/voxmodel.c \
     build/src/sdlayer.c
 
-JMACT_SRC=source/jmact/file_lib.c \
+JMACT_SRC = \
+    source/jmact/file_lib.c \
     source/jmact/control.c \
     source/jmact/keyboard.c \
     source/jmact/mouse.c \
@@ -68,7 +71,8 @@ JMACT_SRC=source/jmact/file_lib.c \
     source/jmact/scriplib.c \
     source/jmact/animlib.c
 
-GAME_SRC=source/game.c \
+GAME_SRC = \
+    source/game.c \
     source/actors.c \
     source/anim.c \
     source/animsounds.c \
@@ -101,8 +105,9 @@ GAME_SRC=source/game.c \
     source/screentext.c \
     source/cheats.c \
     source/sbar.c
- 
- JAUDIO_SRC=source/jaudiolib/src/drivers.c \
+
+JAUDIO_SRC = \
+    source/jaudiolib/src/drivers.c \
     source/jaudiolib/src/fx_man.c \
     source/jaudiolib/src/multivoc.c \
     source/jaudiolib/src/mix.c \
@@ -114,9 +119,9 @@ GAME_SRC=source/game.c \
     source/jaudiolib/src/xa.c \
     source/jaudiolib/src/driver_nosound.c \
     source/jaudiolib/src/driver_sdl.c
- 
- 
- ENET_SRC=source/enet/src/callbacks.c \
+
+ENET_SRC = \
+    source/enet/src/callbacks.c \
     source/enet/src/host.c \
     source/enet/src/list.c \
     source/enet/src/packet.c \
@@ -124,11 +129,11 @@ GAME_SRC=source/game.c \
     source/enet/src/protocol.c \
     source/enet/src/compress.c \
     source/enet/src/unix.c
- 
+
 LOCAL_SRC_FILES         = $(ANDROID_SRC) $(JAUDIO_SRC) $(JMACT_SRC) $(GAME_SRC) $(BUILD_SRC)
 
-LOCAL_LDLIBS            := -lGLESv1_CM -lEGL -ldl -llog 
-LOCAL_STATIC_LIBRARIES  := touch 
-LOCAL_SHARED_LIBRARIES  := ogg vorbis SDL2 SDL2_mixer libvpx 
+LOCAL_LDLIBS            := -lGLESv1_CM -lEGL -ldl -llog
+LOCAL_STATIC_LIBRARIES  := touch
+LOCAL_SHARED_LIBRARIES  := ogg vorbis SDL2 SDL2_mixer libvpx
 
 include $(BUILD_SHARED_LIBRARY)
