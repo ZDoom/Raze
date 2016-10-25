8a995219de8220214f116b95df76fdc6ac3adb97
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2016-10-25 05:43:46 +0000

    Amend r5910 so that the problem is mitigated even when the map load takes place with a different renderer.
    git-svn-id: https://svn.eduke32.com/eduke32@5913 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/build/src/engine.c
+++ polymer/eduke32/build/src/engine.c
@@ -8938,8 +8938,7 @@ static void prepare_loadboard(int32_t fil, vec3_t *dapos, int16_t *daang, int16_
     Bmemset(wallchanged, 0, sizeof(wallchanged));
 
 #ifdef USE_OPENGL
-    if (getrendermode() == REND_POLYMOST)
-        Polymost_prepare_loadboard();
+    Polymost_prepare_loadboard();
 #endif
 
     if (!have_maptext())
