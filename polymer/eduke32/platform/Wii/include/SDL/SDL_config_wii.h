c90ef554bec354abdf0086888f1791303df185a0
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2017-01-01 13:23:23 +0000

    Make SDL_config_wii.h #include <stdint.h> instead of defining its own types, because devkitPPC r28 changed the definitions and/or added an error for when they conflict.
    git-svn-id: https://svn.eduke32.com/eduke32@5978 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/platform/Wii/include/SDL/SDL_config_wii.h
+++ polymer/eduke32/platform/Wii/include/SDL/SDL_config_wii.h
@@ -28,16 +28,7 @@
 #include <stdarg.h>
 
 /* Types */
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef signed short int16_t;
-typedef unsigned short uint16_t;
-typedef signed int int32_t;
-typedef unsigned int uint32_t;
-typedef signed long long int64_t;
-typedef unsigned long long uint64_t;
-typedef unsigned int size_t;
-typedef unsigned int uintptr_t;
+#include <stdint.h>
 
 /* Architecture */
 #define SDL_BYTEORDER		SDL_BIG_ENDIAN
