7dcd59b35b12cef1b7e26a7080399881496b8a12
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2017-01-18 22:22:00 +0000

    Remove some unused definitions. DONT_BUILD.
    git-svn-id: https://svn.eduke32.com/eduke32@6015 1a8010ca-5511-0410-912e-c29ae57300e0

--- polymer/eduke32/source/gamedefs.h
+++ polymer/eduke32/source/gamedefs.h
@@ -34,37 +34,26 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 extern "C" {
 #endif
 
-//****************************************************************************
-//
-// DEFINES
-//
-//****************************************************************************
-
-//
-// Setup program defines
-//
+// config file name
 #define SETUPFILENAME APPBASENAME ".cfg"
 
-// Number of JOY buttons
 // KEEPINSYNC source/jmact/_control.h, build/src/sdlayer.c
 #define MAXJOYBUTTONS 32
 #define MAXJOYBUTTONSANDHATS (MAXJOYBUTTONS+4)
 
-// Number of Mouse Axes
 // KEEPINSYNC source/jmact/_control.h, build/src/sdlayer.c
 #define MAXMOUSEAXES 2
 #define MAXMOUSEDIGITAL (MAXMOUSEAXES*2)
 
-// Number of JOY axes
 // KEEPINSYNC source/jmact/_control.h, build/src/sdlayer.c
 #define MAXJOYAXES 9
 #define MAXJOYDIGITAL (MAXJOYAXES*2)
 
-// DEFAULT mouse sensitivity scale
+// default mouse sensitivity and scale
 #define DEFAULTMOUSESENSITIVITY             7
 #define DEFAULTMOUSEANALOGUESCALE           65536
 
-// DEFAULT joystick settings
+// default joystick settings
 
 #if defined(GEKKO)
 #define DEFAULTJOYSTICKANALOGUESCALE        16384
@@ -75,61 +64,6 @@ extern "C" {
 #define DEFAULTJOYSTICKANALOGUEDEAD         1000
 #define DEFAULTJOYSTICKANALOGUESATURATE     9500
 #endif
-
-enum
-   {
-   gametype_network=3,
-   gametype_serial=1,
-   gametype_modem=2
-   };
-
-enum
-   {
-   connecttype_dialing=0,
-   connecttype_answer=1,
-   connecttype_alreadyconnected=2
-   };
-
-enum
-   {
-   screenbuffer_320x200,
-   screenbuffer_640x400,
-   screenbuffer_640x480,
-   screenbuffer_800x600,
-   screenbuffer_1024x768,
-   screenbuffer_1280x1024,
-   screenbuffer_1600x1200
-   };
-
-enum
-   {
-   vesa_320x200,
-   vesa_360x200,
-   vesa_320x240,
-   vesa_360x240,
-   vesa_320x400,
-   vesa_360x400,
-   vesa_640x350,
-   vesa_640x400,
-   vesa_640x480,
-   vesa_800x600,
-   vesa_1024x768,
-   vesa_1280x1024,
-   vesa_1600x1200
-   };
-
-enum
-   {
-   screenmode_chained,
-   screenmode_vesa,
-   screenmode_buffered,
-   screenmode_tseng,
-   screenmode_paradise,
-   screenmode_s3,
-   screenmode_crystal,
-   screenmode_redblue,
-   };
-
 
 #ifdef __cplusplus
 }
