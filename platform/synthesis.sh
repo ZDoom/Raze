5d77a4ac0668d2e04f3b4946f1dc1571c8cad281
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2017-02-03 23:03:06 +0000

    Fix compiler specified for synthesis. DONT_BUILD.
    git-svn-id: https://svn.eduke32.com/eduke32@6060 1a8010ca-5511-0410-912e-c29ae57300e0

--- platform/synthesis.sh
+++ platform/synthesis.sh
@@ -8,8 +8,8 @@ top=/var/www/synthesis
 lockfile=$top/synthesis_building
 source=$top/eduke32
 output=/var/www/dukeworld.duke4.net/eduke32/synthesis
-make=( make SYNTHESIS=1 PLATFORM=WINDOWS CROSS='i686-w64-mingw32-' CC='i686-w64-mingw32-gcc-5.4-win32' CXX='i686-w64-mingw32-g++-5.4-win32' AS='nasm' PRETTY_OUTPUT=0 SDLCONFIG='' )
-make64=( make SYNTHESIS=1 PLATFORM=WINDOWS CROSS='x86_64-w64-mingw32-' CC='x86_64-w64-mingw32-gcc-5.4-win32' CXX='x86_64-w64-mingw32-g++-5.4-win32' AS='nasm' PRETTY_OUTPUT=0 SDLCONFIG='' )
+make=( make SYNTHESIS=1 PLATFORM=WINDOWS CROSS='i686-w64-mingw32-' CC='i686-w64-mingw32-gcc' CXX='i686-w64-mingw32-g++' AS='nasm' PRETTY_OUTPUT=0 SDLCONFIG='' )
+make64=( make SYNTHESIS=1 PLATFORM=WINDOWS CROSS='x86_64-w64-mingw32-' CC='x86_64-w64-mingw32-gcc' CXX='x86_64-w64-mingw32-g++' AS='nasm' PRETTY_OUTPUT=0 SDLCONFIG='' )
 clean=veryclean
 
 # the following file paths are relative to $source  
