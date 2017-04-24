80ebabd08a7eb4639cbe31518e2bd7507bac475c
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2017-04-24 05:11:08 +0000

    Amend r6122, as of r6074 all standard library header inclusion is routed through compat.h.
    git-svn-id: https://svn.eduke32.com/eduke32@6128 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/duke3d/src/midi.cpp
+++ source/duke3d/src/midi.cpp
@@ -32,8 +32,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **********************************************************************/
 
 #include "compat.h"
-#include <stdlib.h>
-#include <string.h>
 #include "music.h"
 #include "_midi.h"
 #include "midi.h"
