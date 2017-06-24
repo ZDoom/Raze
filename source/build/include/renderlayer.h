854b303d18dac26cde1dbb4a540aefa266889c2c
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2017-06-24 21:17:59 +0000

    Fix command line help window in builds with the startup window disabled
    git-svn-id: https://svn.eduke32.com/eduke32@6270 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/build/include/renderlayer.h
+++ source/build/include/renderlayer.h
@@ -11,9 +11,9 @@
 #if defined _WIN32 || defined EDUKE32_OSX || defined HAVE_GTK2
 # define STARTUP_SETUP_WINDOW
 #endif
+#endif
 
 #undef WM_MSGBOX_WINDOW
 #if defined _WIN32 || defined EDUKE32_OSX || defined HAVE_GTK2 || (defined RENDERTYPESDL && SDL_MAJOR_VERSION >= 2)
 # define WM_MSGBOX_WINDOW
-#endif
 #endif
