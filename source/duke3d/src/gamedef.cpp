4cea9ec804ea12e63f0e4c77152895b437751a89
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2018-02-10 22:58:38 +0000

    Fix MSVC build breakage.
    git-svn-id: https://svn.eduke32.com/eduke32@6642 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/duke3d/src/gamedef.cpp
+++ source/duke3d/src/gamedef.cpp
@@ -2774,7 +2774,7 @@ void C_InitQuotes(void)
 #ifdef EDUKE32_TOUCH_DEVICES
     apStrings[QUOTE_DEAD] = 0;
 #else
-    static constexpr char const * const OpenGameFunc = gamefunctions[gamefunc_Open];
+    char const * const OpenGameFunc = gamefunctions[gamefunc_Open];
     C_ReplaceQuoteSubstring(QUOTE_DEAD, "SPACE", OpenGameFunc);
     C_ReplaceQuoteSubstring(QUOTE_DEAD, "OPEN", OpenGameFunc);
     C_ReplaceQuoteSubstring(QUOTE_DEAD, "USE", OpenGameFunc);
