920ec3f6a26b75a64265637b97ed3934f524c7ec
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2018-03-01 13:46:48 +0000

    Fix the position of the notch when scaling the slidebar with the mouse.
    Patch from Fox.
    
    git-svn-id: https://svn.eduke32.com/eduke32@6713 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/duke3d/src/menus.cpp
+++ source/duke3d/src/menus.cpp
@@ -4765,7 +4765,7 @@ static int32_t M_RunMenu_Menu(Menu_t *cm, MenuMenu_t *menu, MenuEntry_t *current
 
                             if (!m_mousecaught && (mousepressstate == Mouse_Pressed || mousepressstate == Mouse_Held))
                             {
-                                const int32_t slidepointhalfwidth = mulscale16((2+tilesiz[SLIDEBAR+1].x)<<15, z);
+                                const int32_t slidepointhalfwidth = mulscale16((((tilesiz[SLIDEBAR+1].x)*ud.menu_slidecursorz)>>2) + ud.menu_slidebarmargin, z);
                                 const int32_t slideregionx = slidebarx + slidepointhalfwidth;
 
                                 menu->currentEntry = e;
