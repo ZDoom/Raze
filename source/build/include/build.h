d20df1ec9a749b70527f45510fccb464f66aafe2
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2018-04-06 01:43:22 +0000

    Bump MAXSPRITESONSCREEN from 2048 to 2560
    git-svn-id: https://svn.eduke32.com/eduke32@6820 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/build/include/build.h
+++ source/build/include/build.h
@@ -103,7 +103,7 @@ enum rendmode_t {
 #define MAXPLAYERS 16
 // Maximum number of component tiles in a multi-psky:
 #define MAXPSKYTILES 8
-#define MAXSPRITESONSCREEN 2048
+#define MAXSPRITESONSCREEN 2560
 #define MAXUNIQHUDID 256 //Extra slots so HUD models can store animation state without messing game sprites
 
 #define TSPR_TEMP 99
