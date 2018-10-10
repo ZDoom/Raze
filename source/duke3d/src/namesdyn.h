e6c36442bdeae13ec7f330155f05bfc73e3148e1
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2018-10-10 19:14:36 +0000

    Add missing ifdef guards to namesdyn.h
    git-svn-id: https://svn.eduke32.com/eduke32@7054 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/duke3d/src/namesdyn.h
+++ source/duke3d/src/namesdyn.h
@@ -20,6 +20,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 //-------------------------------------------------------------------------
 
+#ifndef namesdyn_h__
+#define namesdyn_h__
+
+
 #define DYNTILEREMAP_ENABLE
 
 
@@ -1241,3 +1245,4 @@ extern int32_t JURYGUY;
 #define DYNAMICTILEMAP(Tilenum) (Tilenum)
 
 #endif
+#endif // namesdyn_h__
