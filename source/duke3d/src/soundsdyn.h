f354f824f4691d36fc2b16bc25d7d8980de1fc90
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2018-10-10 19:14:20 +0000

    Add missing ifdef guards to soundsdyn.h
    git-svn-id: https://svn.eduke32.com/eduke32@7051 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/duke3d/src/soundsdyn.h
+++ source/duke3d/src/soundsdyn.h
@@ -19,6 +19,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 //-------------------------------------------------------------------------
+#ifndef soundsdyn_h__
+#define soundsdyn_h__
 
 #define DYNSOUNDREMAP_ENABLE
 
@@ -328,3 +330,4 @@ extern int32_t BIGBANG;
 #define DYNAMICSOUNDMAP(Soundnum) (Soundnum)
 
 #endif
+#endif // soundsdyn_h__
