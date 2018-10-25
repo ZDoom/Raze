ffab1f7d71a4789bad0a56ef3ead70d017b9bacf
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2018-10-25 23:30:56 +0000

    Slightly simplify TrackedType operator stuff
    git-svn-id: https://svn.eduke32.com/eduke32@7102 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/build/include/tracker_operator.hpp
+++ source/build/include/tracker_operator.hpp
@@ -2,29 +2,23 @@
 template<typename TrackedType>
 inline TrackedType TRACKER_NAME_<TrackedType>::operator TRACKER_OPERATOR_ (__TRACKER_RIGHTHAND_TYPE)
 {
-    bool isNoop;
+    bool isNoop = false;
 
-    switch (TRACKER_NOOP_) {
-        case TRACKER_NOOP_RIGHTHAND_EQUAL_:
-            isNoop = this->TrackedValue == __TRACKER_RIGHTHAND;
-            break;
-        case TRACKER_NOOP_RIGHTHAND_ZERO_:
-            isNoop = __TRACKER_RIGHTHAND == 0;
-            break;
-        case TRACKER_NOOP_RIGHTHAND_ONE_:
-            isNoop = __TRACKER_RIGHTHAND == 1;
-            break;
+    switch (TRACKER_NOOP_)
+    {
+        case TRACKER_NOOP_RIGHTHAND_EQUAL_: isNoop = (this->TrackedValue == __TRACKER_RIGHTHAND); break;
+        case TRACKER_NOOP_RIGHTHAND_ZERO_: isNoop  = (__TRACKER_RIGHTHAND == 0); break;
+        case TRACKER_NOOP_RIGHTHAND_ONE_: isNoop   = (__TRACKER_RIGHTHAND == 1);
+            fallthrough__;
+        // case __TRACKER_NEVER:
         default:
-        case __TRACKER_NEVER:
-            isNoop = false;
             break;
     }
 
-    if (!isNoop) {
-
-        TRACKER_GLOBAL_HOOK_((uintptr_t)&this->TrackedValue);
-        return (this->TrackedValue TRACKER_OPERATOR_ __TRACKER_RIGHTHAND);
-    } else {
-        return this->TrackedValue;
+    if (!isNoop)
+    {
+        TRACKER_GLOBAL_HOOK_((uintptr_t) & this->TrackedValue);
+        return this->TrackedValue TRACKER_OPERATOR_ __TRACKER_RIGHTHAND;
     }
+    else return this->TrackedValue;
 }
