84c43dc99cd7c522e7cc97de75b0df65c427112e
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2019-04-09 19:21:40 +0000

    Fix warnings with TrackedType
    git-svn-id: https://svn.eduke32.com/eduke32@7551 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/build/include/tracker_operator.hpp
+++ source/build/include/tracker_operator.hpp
@@ -7,7 +7,7 @@ inline TrackedType TRACKER_NAME_<TrackedType>::operator TRACKER_OPERATOR_ (Right
 
     switch (TRACKER_NOOP_)
     {
-        case TRACKER_NOOP_RIGHTHAND_EQUAL_: isNoop = (this->TrackedValue == rightHand); break;
+        case TRACKER_NOOP_RIGHTHAND_EQUAL_: isNoop = (this->TrackedValue == (TrackedType)rightHand); break;
         case TRACKER_NOOP_RIGHTHAND_ZERO_: isNoop  = (rightHand == 0); break;
         case TRACKER_NOOP_RIGHTHAND_ONE_: isNoop   = (rightHand == 1);
             fallthrough__;
@@ -19,7 +19,7 @@ inline TrackedType TRACKER_NAME_<TrackedType>::operator TRACKER_OPERATOR_ (Right
     if (!isNoop)
     {
         TRACKER_GLOBAL_HOOK_((uintptr_t) & this->TrackedValue);
-        return this->TrackedValue TRACKER_OPERATOR_ rightHand;
+        return this->TrackedValue TRACKER_OPERATOR_ (TrackedType)rightHand;
     }
     else return this->TrackedValue;
 }
