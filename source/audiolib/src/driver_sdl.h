53cd67b0f202adb3b3d118a9efa72f206088bf6e
terminx <terminx@1a8010ca-5511-0410-912e-c29ae57300e0>
2019-08-09 09:28:18 +0000

    Add ifdef guard
    git-svn-id: https://svn.eduke32.com/eduke32@7931 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/audiolib/src/driver_sdl.h
+++ source/audiolib/src/driver_sdl.h
@@ -18,6 +18,9 @@
 
  */
 
+#ifndef driver_sdl_h__
+#define driver_sdl_h__
+
 #include "compat.h"
 
 int32_t SDLDrv_GetError(void);
@@ -29,3 +32,4 @@ int32_t SDLDrv_PCM_BeginPlayback(char *BufferStart, int32_t BufferSize,
 void SDLDrv_PCM_StopPlayback(void);
 void SDLDrv_PCM_Lock(void);
 void SDLDrv_PCM_Unlock(void);
+#endif // driver_sdl_h__
