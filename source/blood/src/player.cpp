160c93f0ab2ca809d3a51e38af817464521dc52a
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2019-09-17 12:16:05 +0000

    Multiplayer suicide message was empty string (#170)
    # Conflicts:
    #	.gitignore

--- source/blood/src/player.cpp
+++ source/blood/src/player.cpp
@@ -2033,6 +2033,10 @@ void playerFrag(PLAYER *pKiller, PLAYER *pVictim)
             if (gGameOptions.nGameType > 0 && nSound >= 0)
                 sndStartSample(nSound, 255, 2, 0);
         }
+        else
+        {
+            sprintf(buffer, gSuicide[nMessage].at0, gProfile[nVictim].name);
+        }
     }
     else
     {
