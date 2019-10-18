8b1f2f5fc9960d9d47271c7b7e738f1aa8a1e276
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-10-19 00:27:34 +0200

    - disabled console lockout option as a modding feature.
    No, sorry, but this is something I consider unacceptable!

--- source/duke3d/src/game.cpp
+++ source/duke3d/src/game.cpp
@@ -6576,7 +6576,7 @@ int app_main(int argc, char const * const * argv)
 
     Bsprintf(tempbuf, HEAD2 " %s", s_buildRev);
     OSD_SetVersion(tempbuf, 10,0);
-    OSD_SetParameters(0, 0, 0, 12, 2, 12, OSD_ERROR, OSDTEXT_RED, gamefunctions[gamefunc_Show_Console][0] == '\0' ? OSD_PROTECTED : 0);
+    OSD_SetParameters(0, 0, 0, 12, 2, 12, OSD_ERROR, OSDTEXT_RED, /*gamefunctions[gamefunc_Show_Console][0] == '\0' ? OSD_PROTECTED :*/ 0);
     registerosdcommands();
 
     if (g_networkMode != NET_DEDICATED_SERVER)
