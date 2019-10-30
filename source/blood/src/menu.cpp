bc6c1bbc3c503b8c13163dd4916a5e20e3870dbe
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2019-10-30 05:05:24 +0000

    Fix the game on non-Windows (#240)

--- source/blood/src/menu.cpp
+++ source/blood/src/menu.cpp
@@ -603,7 +603,7 @@ CGameMenuItemSlider itemOptionsSoundMusicVolume("MUSIC VOLUME:", 3, 66, 100, 180
 CGameMenuItemZCycle itemOptionsSoundSampleRate("SAMPLE RATE:", 3, 66, 110, 180, 0, UpdateSoundRate, pzSoundRateStrings, 3, 0);
 CGameMenuItemSlider itemOptionsSoundNumVoices("VOICES:", 3, 66, 120, 180, snd_numvoices, 16, 256, 16, UpdateNumVoices, -1, -1, kMenuSliderValue);
 CGameMenuItemZBool itemOptionsSoundCDToggle("REDBOOK AUDIO:", 3, 66, 130, 180, false, UpdateCDToggle, NULL, NULL);
-CGameMenuItemZCycle itemOptionsSoundMusicDevice("MUSIC DEVICE:", 3, 66, 140, 180, 0, UpdateMusicDevice, pzMusicDeviceStrings, 2, 0);
+CGameMenuItemZCycle itemOptionsSoundMusicDevice("MUSIC DEVICE:", 3, 66, 140, 180, 0, UpdateMusicDevice, pzMusicDeviceStrings, ARRAY_SIZE(pzMusicDeviceStrings), 0);
 CGameMenuItemChain itemOptionsSoundApplyChanges("APPLY CHANGES", 3, 66, 150, 180, 0, NULL, 0, SetSound, 0);
 
 
@@ -1876,7 +1876,7 @@ void SetupOptionsSound(CGameMenuItemChain *pItem)
     }
     itemOptionsSoundNumVoices.nValue = snd_numvoices;
     itemOptionsSoundMusicDevice.m_nFocus = 0;
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < ARRAY_SIZE(nMusicDeviceValues); i++)
     {
         if (nMusicDeviceValues[i] == MusicDevice)
         {
