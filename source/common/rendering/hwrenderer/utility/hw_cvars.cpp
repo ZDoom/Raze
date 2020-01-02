afb1d7b8857bea4aa395a74673e929aa1719c8d9
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-01-02 19:38:47 +0100

    - changed a few licenses of my own code to something more permissive.

--- source/common/rendering/hwrenderer/utility/hw_cvars.cpp
+++ source/common/rendering/hwrenderer/utility/hw_cvars.cpp
@@ -1,44 +1,15 @@
-// 
-//---------------------------------------------------------------------------
-//
-// Copyright(C) 2005-2016 Christoph Oelckers
-// All rights reserved.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/
-//
-//--------------------------------------------------------------------------
-//
-
-
 #include "c_cvars.h"
 #include "c_dispatch.h"
 #include "v_video.h"
 #include "hw_cvars.h"
 #include "menu/menu.h"
 
-
-
-// OpenGL stuff moved here
-// GL related CVARs
-
-
 //==========================================================================
 //
 // Texture CVARs
 //
 //==========================================================================
-#if 0
+#if 0 // left as a reminder that this will have to be refactored later.
 CUSTOM_CVAR(Float,gl_texture_filter_anisotropic,8.0f,CVAR_ARCHIVE|CVAR_GLOBALCONFIG|CVAR_NOINITCALL)
 {
 	screen->TextureFilterChanged();
@@ -57,30 +28,3 @@ CUSTOM_CVAR(Int, gl_texture_filter, 4, CVAR_ARCHIVE|CVAR_GLOBALCONFIG|CVAR_NOINI
 CVAR(Bool, gl_precache, false, CVAR_ARCHIVE)
 #endif
 
-
-//==========================================================================
-//
-// Sprite CVARs
-//
-//==========================================================================
-#if 0
-CVAR(Bool, gl_usecolorblending, true, CVAR_ARCHIVE|CVAR_GLOBALCONFIG)
-CVAR(Bool, gl_sprite_blend, false, CVAR_ARCHIVE|CVAR_GLOBALCONFIG);
-CVAR(Int, gl_spriteclip, 1, CVAR_ARCHIVE)
-CVAR(Float, gl_sclipthreshold, 10.0, CVAR_ARCHIVE)
-CVAR(Float, gl_sclipfactor, 1.8f, CVAR_ARCHIVE)
-CVAR(Int, gl_particles_style, 2, CVAR_ARCHIVE | CVAR_GLOBALCONFIG) // 0 = square, 1 = round, 2 = smooth
-CVAR(Int, gl_billboard_mode, 0, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
-CVAR(Bool, gl_billboard_faces_camera, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
-CVAR(Bool, gl_billboard_particles, true, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
-CVAR(Int, gl_enhanced_nv_stealth, 3, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
-CUSTOM_CVAR(Int, gl_fuzztype, 0, CVAR_ARCHIVE)
-{
-	if (self < 0 || self > 8) self = 0;
-}
-
-CUSTOM_CVAR(Int, gl_shadowmap_filter, 1, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
-{
-	if (self < 0 || self > 8) self = 1;
-}
-#endif
