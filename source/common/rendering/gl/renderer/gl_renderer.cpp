e923ef5d3acbb0a1596094afdf836f02ae554480
Magnus Norddahl <dpjudas@users.noreply.github.com>
2020-01-14 14:22:03 +0100

    Add missing M_FinishPNG call

--- source/common/rendering/gl/renderer/gl_renderer.cpp
+++ source/common/rendering/gl/renderer/gl_renderer.cpp
@@ -239,6 +239,7 @@ void FGLRenderer::WriteSavePic ( FileWriter *file, int width, int height)
 		uint8_t* scr = (uint8_t*)Xmalloc(numpixels * 3);
 		glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, scr);
 		M_CreatePNG(file, scr + ((height - 1) * width * 3), nullptr, SS_RGB, width, height, -width * 3, vid_gamma);
+        M_FinishPNG(file);
 		Xfree(scr);
 	}
     
