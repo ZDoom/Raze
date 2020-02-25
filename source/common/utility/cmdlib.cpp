1c2ea506f26653adbfff06bd4fceb4ab4b44ec10
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-02-25 18:49:24 +0100

    - fixed: Windows must call the wide version of _mkdir for Unicode support.

--- source/common/utility/cmdlib.cpp
+++ source/common/utility/cmdlib.cpp
@@ -441,7 +441,12 @@ void DoCreatePath(const char *fn)
 	if ('\0' != *path)
 	{
 		DoCreatePath(path);
+#ifdef _WIN32
+		auto wpath = WideString(path);
+		_wmkdir(wpath.c_str());
+#else
 		_mkdir(path);
+#endif
 	}
 }
 
