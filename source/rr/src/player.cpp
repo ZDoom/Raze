a0bf194ab477cec71cc0ea48565f8a559aaccfa7
Mitchell Richters <mjr4077au@gmail.com>
2020-03-18 10:11:50 +1100

    Manually merge parts of EDuke32 SVN commit 8243.

--- source/rr/src/player.cpp
+++ source/rr/src/player.cpp
@@ -8160,7 +8160,6 @@ check_enemy_sprite:
                 pPlayer->pos.z = floorZ - (floorZOffset << 8);
             else
             {
-                pPlayer->on_ground = 0;
                 if (RRRA && (pPlayer->on_motorcycle || pPlayer->on_boat) && floorZ - (floorZOffset << 9) > pPlayer->pos.z)
                 {
                     if (pPlayer->moto_on_ground)
@@ -8236,7 +8235,10 @@ check_enemy_sprite:
                             pPlayer->moto_turb = 12;
                         }
                     }
+                    pPlayer->on_ground = 1;
                 }
+                else
+                    pPlayer->on_ground = 0;
             }
         }
         else
