53c9d8425c93ba437d4ea6317cdacb335f0cd480
Mitchell Richters <mjr4077au@gmail.com>
2020-03-18 10:07:31 +1100

    Manually merge EDuke32 SVN commit 8555.

--- source/rr/src/player.h
+++ source/rr/src/player.h
@@ -228,9 +228,14 @@ typedef struct {
 } DukePlayer_t;
 
 // KEEPINSYNC lunatic/_defs_game.lua
-typedef struct {
+typedef struct
+{
     DukePlayer_t *ps;
     input_t *input;
+
+    bool    horizRecenter;
+    float   horizAngleAdjust;
+    fix16_t horizSkew;
 
     int32_t movefifoend, syncvalhead, myminlag;
     int32_t pcolor, pteam;
