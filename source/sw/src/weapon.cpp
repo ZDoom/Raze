537313f620dc98a832d862aff86a78e0b3b3cb23
NY00123 <ny00@outlook.com>
2020-04-04 00:43:51 +0300

    SW: Revert commented out horiz->q16horiz renames in DSPRINTF strings

--- source/sw/src/weapon.cpp
+++ source/sw/src/weapon.cpp
@@ -19312,7 +19312,7 @@ InitGrenade(PLAYERp pp)
 
     wp->zvel = ((100 - fix16_to_int(pp->q16horiz)) * HORIZ_MULT);
 
-    ////DSPRINTF(ds,"q16horiz %d, ho %d, ho+ho %d",fix16_to_int(pp->q16horiz), fix16_to_int(pp->q16horizoff), fix16_to_int(pp->q16horizoff + pp->q16horiz));
+    ////DSPRINTF(ds,"horiz %d, ho %d, ho+ho %d",fix16_to_int(pp->q16horiz), fix16_to_int(pp->q16horizoff), fix16_to_int(pp->q16horizoff + pp->q16horiz));
     //MONO_PRINT(ds);
 
     oclipdist = pp->SpriteP->clipdist;
