588162e08320aa3c07db41ec09bed2f920b65e60
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-04-11 23:44:22 +0200

    - removed redundant MAKE_ID definition.

--- source/core/utility/m_png.h
+++ source/core/utility/m_png.h
@@ -37,6 +37,7 @@
 #include "zstring.h"
 #include "files.h"
 #include "palentry.h"
+#include "basics.h"
 
 // Screenshot buffer image data types
 enum ESSType
@@ -121,13 +122,5 @@ bool M_ReadIDAT (FileReader &file, uint8_t *buffer, int width, int height, int p
 class FTexture;
 
 FTexture *PNGTexture_CreateFromFile(PNGHandle *png, const FString &filename);
-
-#ifndef MAKE_ID
-#ifndef __BIG_ENDIAN__
-#define MAKE_ID(a,b,c,d)	((uint32_t)((a)|((b)<<8)|((c)<<16)|((d)<<24)))
-#else
-#define MAKE_ID(a,b,c,d)	((uint32_t)((d)|((c)<<8)|((b)<<16)|((a)<<24)))
-#endif
-#endif 
 
 #endif
