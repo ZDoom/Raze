bd6f63c227d2689b580bbc8102daf54a09a8a454
Mitchell Richters <mjr4077au@gmail.com>
2020-05-07 16:39:58 +1000

    Duke 3D & RR: Repair issue where player would not return to centre following hard landing.
    - Accidentally left in while merging changes from upstream.
    - Change restores accuracy to game play in that a hard landing now returns the player's view to center.

--- source/duke3d/src/player.cpp
+++ source/duke3d/src/player.cpp
@@ -5301,7 +5301,6 @@ void P_ProcessInput(int playerNum)
                             A_PlaySound(DUKE_LAND, pPlayer->i);
 #endif
                     }
-                    pPlayer->on_ground = 1;
                 }
                 else
                     pPlayer->on_ground = 0;
