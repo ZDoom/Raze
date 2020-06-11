6ececaec40fd11ca9aa10654faf72b7df926c62b
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-06-12 00:43:40 +0200

    - include cleanup

--- source/rr/src/anim.cpp
+++ source/rr/src/anim.cpp
@@ -31,7 +31,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "v_2ddrawer.h"
 #include "animtexture.h"
 #include "v_video.h"
-#include "../glbackend/glbackend.h"
 
 
 #include "anim.h"
