6be1a9a9e46e997fcabcca23b07464bbf440ac9d
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-06-28 09:03:31 +0200

    - copyright

--- source/games/duke/src/flags_r.cpp
+++ source/games/duke/src/flags_r.cpp
@@ -2,6 +2,7 @@
 /*
 Copyright (C) 1996, 2003 - 3D Realms Entertainment
 Copyright (C) 2017-2019 Nuke.YKT
+Copyright (C) 2020 - Christoph Oelckers
 
 This file is part of Duke Nukem 3D version 1.5 - Atomic Edition
 
