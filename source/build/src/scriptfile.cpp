2f3d405f8b8c9768f2039519ceefb8476ca06826
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-07-14 19:51:54 +0200

    - moved getatoken to scriptfile.cpp so common.cpp could be removed.

--- source/build/src/scriptfile.cpp
+++ source/build/src/scriptfile.cpp
@@ -438,3 +438,21 @@ void scriptfile_clearsymbols(void)
     symbtablength = 0;
     symbtaballoclength = 0;
 }
+
+int32_t getatoken(scriptfile *sf, const tokenlist *tl, int32_t ntokens)
+{
+    char *tok;
+    int32_t i;
+
+    if (!sf) return T_ERROR;
+    tok = scriptfile_gettoken(sf);
+    if (!tok) return T_EOF;
+
+    for (i=ntokens-1; i>=0; i--)
+    {
+        if (!Bstrcasecmp(tok, tl[i].text))
+            return tl[i].tokenid;
+    }
+    return T_ERROR;
+}
+
