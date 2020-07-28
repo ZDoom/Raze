9b51e29d20e2830cb41b8a377117396bae72acb1
Mitchell Richters <mjr4077au@gmail.com>
2020-07-29 05:50:06 +1000

    Revert "Revert "- removed duplicate application in moving down code for the jetpack.". What was I thinking?"
    This reverts commit 3b522e4c50ea8a36998cc772dfb754a93e70b5cb.

--- source/games/duke/src/player_d.cpp
+++ source/games/duke/src/player_d.cpp
@@ -1595,8 +1595,6 @@ static void operateJetpack(int snum, ESyncBits sb_snum, int psectlotag, int fz, 
 			p->posz += j;
 			p->crack_time = 777;
 		}
-		p->posz += j;
-		p->crack_time = 777;
 	}
 
 	int k;
