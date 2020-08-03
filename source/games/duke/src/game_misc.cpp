aead65183c1a4dae85119c6328a7b0ac00f84319
Mitchell Richters <mjr4077au@gmail.com>
2020-08-03 22:56:43 +1000

    - draw the crosshair using `getHalfLookAng()` inline function, making use of its double precision and optionally interpolated result.

--- source/games/duke/src/game_misc.cpp
+++ source/games/duke/src/game_misc.cpp
@@ -358,7 +358,7 @@ void displayrest(double smoothratio)
 			double crosshair_scale = cl_crosshairscale * .01;
 			if (isRR()) crosshair_scale *= .5;
 
-			DrawTexture(twod, tileGetTexture(a), 160 - (ps[myconnectindex].getlookang() >> 1), 100,
+			DrawTexture(twod, tileGetTexture(a), 160 - getHalfLookAng(screenpeek, cl_syncinput, smoothratio), 100,
 				DTA_FullscreenScale, 3, DTA_VirtualWidth, 320, DTA_VirtualHeight, 200, DTA_ScaleX, crosshair_scale, DTA_ScaleY, crosshair_scale, DTA_CenterOffsetRel, true,
 				DTA_ViewportX, windowxy1.x, DTA_ViewportY, windowxy1.y, DTA_ViewportWidth, windowxy2.x - windowxy1.x + 1, DTA_ViewportHeight, windowxy2.y - windowxy1.y + 1, TAG_DONE);
 		}
