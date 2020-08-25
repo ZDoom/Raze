576b13248eb80fc9f247ed6fe805af5bccd8c6b8
Mitchell Richters <mjr4077au@gmail.com>
2020-08-26 07:57:27 +1000

    - Build: Use `I_GetBuildTime()` in place of `totalclocklock` in `animateoffs()`. This makes the SW menu work pre-game.

--- source/build/src/engine.cpp
+++ source/build/src/engine.cpp
@@ -447,7 +447,7 @@ int32_t animateoffs(int const tilenum, int fakevar)
     if (animnum <= 0)
         return 0;
 
-    int const i = (int) totalclocklock >> (picanm[tilenum].sf & PICANM_ANIMSPEED_MASK);
+    int const i = (int) I_GetBuildTime() >> (picanm[tilenum].sf & PICANM_ANIMSPEED_MASK);
     int offs = 0;
 
     switch (picanm[tilenum].sf & PICANM_ANIMTYPE_MASK)
