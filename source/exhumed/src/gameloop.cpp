e5a4276a5a8ba255136eaedf3507c9573e30c02a
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-08-26 20:20:36 +0200

    - fixed infinite loop.

--- source/exhumed/src/gameloop.cpp
+++ source/exhumed/src/gameloop.cpp
@@ -83,8 +83,8 @@ static int FinishLevel(TArray<JobDesc> &jobs)
         {
             // There's really no choice but to enter an active wait loop here to make the sound play out.
             PlayLocalSound(StaticSound[59], 0, true, CHANF_UI);
-            int nTicks = gameclock + 12;
-            while (nTicks > gameclock) 
+            int nTicks = I_msTime() + 100;
+            while (nTicks > I_msTime()) 
             { 
                 I_GetEvent();
                 soundEngine->UpdateSounds(I_GetTime());
