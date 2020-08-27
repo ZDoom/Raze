aeb807bea1897663a9aebf0cb9f9844ae365c109
Mitchell Richters <mjr4077au@gmail.com>
2020-08-27 22:42:50 +1000

    - gamecvars.cpp: Create CVAR `cl_showmagamt`.

--- source/core/gamecvars.cpp
+++ source/core/gamecvars.cpp
@@ -81,6 +81,7 @@ CVARD(Bool, cl_syncinput, false, CVAR_ARCHIVE, "enable/disable synchronized inpu
 CVARD(Bool, cl_swaltnukeinit, false, CVAR_ARCHIVE, "enable/disable SW alternative nuke initialisation") // only implemented in SW
 CVARD(Bool, cl_dukefixrpgrecoil, false, CVAR_ARCHIVE, "soften recoil of Duke 3D's RPG")
 CVARD(Bool, cl_smoothsway, false, CVAR_ARCHIVE, "move SW weapon left and right smoothly while bobbing")
+CVARD(Bool, cl_showmagamt, false, CVAR_ARCHIVE, "show the amount of rounds left in the magazine of your weapon on the modern HUD")
 CUSTOM_CVARD(Int, cl_crosshairscale, 50, CVAR_ARCHIVE, "changes the size of the crosshair")
 {
 	if (self < 1) self = 1;
