ZDoom/Raze

commit 847fa48724f8eefae26d52e1f6c5ab599ba5e84f

- cleaned out the remaining parts of baselayer.h
In particular this means to remove the option to disable widescreen aspect ratios. The way this was handled makes no sense with the current render backend.

The aspect ratio code will have to be redone entirely to properly obey the backend's settings.

source/build/src/animvpx.cpp
@@ -5,7 +5,6 @@
 #include "compat.h"
 
 #include "compat.h"
-#include "baselayer.h"
 #include "build.h"
 
 #include "matrix.h"

source/blood/src/blood.cpp
@@ -26,7 +26,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "build.h"
 #include "mmulti.h"
 #include "compat.h"
-#include "baselayer.h"
 #include "common.h"
 #include "common_game.h"
 #include "g_input.h"

source/blood/src/blood.h
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "misc.h"
 #include "db.h"
 #include "mapinfo.h"
+#include "gamestruct.h"
 
 BEGIN_BLD_NS
 

source/blood/src/common_game.h
@@ -21,7 +21,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 //-------------------------------------------------------------------------
 #pragma once
-#include "baselayer.h"
 #include "build.h"
 
 #include "common.h"

source/blood/src/controls.cpp
@@ -24,7 +24,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "ns.h"	// Must come before everything else!
 
 #include "compat.h"
-#include "baselayer.h"
 #include "mmulti.h"
 #include "gamecontrol.h"
 #include "common_game.h"

source/blood/src/osdcmd.cpp
@@ -24,7 +24,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "ns.h"	// Must come before everything else!
 
 #include "build.h"
-#include "baselayer.h"
 #include "compat.h"
 #include "mmulti.h"
 #include "common_game.h"

source/blood/src/sound.cpp
@@ -27,7 +27,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "common_game.h"
 #include "levels.h"
 #include "sound.h"
-#include "baselayer.h"
 #include "raze_music.h"
 #include "raze_sound.h"
 #include "gamecontrol.h"

source/blood/src/view.cpp
@@ -690,11 +690,7 @@ void viewDrawScreen(bool sceneonly)
 
         int yxAspect = yxaspect;
         int viewingRange = viewingrange;
-        if (r_usenewaspect)
-        {
-            newaspect_enable = 1;
-            videoSetCorrectedAspect();
-        }
+        videoSetCorrectedAspect();
 
         int v1 = Blrintf(double(viewingrange) * tan(r_fov * (PI / 360.)));
 
@@ -1004,8 +1000,6 @@ void viewDrawScreen(bool sceneonly)
 
         bDeliriumOld = bDelirium && gDeliriumBlur;
 
-        if (r_usenewaspect)
-            newaspect_enable = 0;
         renderSetAspect(viewingRange, yxAspect);
         int nClipDist = gView->pSprite->clipdist << 2;
         int ve8, vec, vf0, vf4;

commit c47b60e1930ea3bd8dd64deaffe70236eab72db7

- Fix compile regression from 847fa48724f8eefae26d52e1f6c5ab599ba5e84f.
Fixes #283.

source/build/src/animvpx.cpp
@@ -6,7 +6,7 @@
 
 #include "compat.h"
 #include "build.h"
-
+#include "printf.h"
 #include "matrix.h"
 #include "../../glbackend/glbackend.h"
 #include "textures.h"