7c5635671eeb288db728b26cb00a46b352d53f1d
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-09-05 12:01:47 +0200

    - Exhumed: When Ramses is talking, hide the status bar.

--- source/exhumed/src/status.cpp
+++ source/exhumed/src/status.cpp
@@ -843,6 +843,7 @@ void StatusMessage(int messageTime, const char* fmt, ...)
 
 void DrawStatusBar()
 {
+    if (nFreeze == 2) return; // Hide when Ramses is talking.
     if (hud_size <= Hud_Stbar)
     {
         UpdateFrame();
