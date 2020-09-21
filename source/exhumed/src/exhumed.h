8a4d4b6905d38cd05be3844f74e5cbde32f3f991
Mitchell Richters <mjr4077au@gmail.com>
2020-09-21 18:48:38 +1000

    - Exhumed: Open game's constrained horizon as much as sky tile in first level permits.

--- source/exhumed/src/exhumed.h
+++ source/exhumed/src/exhumed.h
@@ -260,8 +260,8 @@ struct GameInterface : ::GameInterface
 	void LevelCompleted(MapRecord *map, int skill) override;
 	void NextLevel(MapRecord *map, int skill) override;
     bool DrawAutomapPlayer(int x, int y, int z, int a) override;
-    fixed_t playerHorizMin() override { return 0; }
-    fixed_t playerHorizMax() override { return IntToFixed(184); }
+    fixed_t playerHorizMin() override { return IntToFixed(-50); }
+    fixed_t playerHorizMax() override { return IntToFixed(250); }
 
 	::GameStats getStats() override;
 };
