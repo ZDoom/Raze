babd7b955628e22cf40a8bedf9bffd3e6c9bd062
Mitchell Richters <mjr4077au@gmail.com>
2020-10-07 23:25:36 +1100

    - InputState: Proposed new sensitivity dividers now that mouse input is based on true pitch and true angle.

--- source/core/inputstate.cpp
+++ source/core/inputstate.cpp
@@ -62,8 +62,8 @@ CVAR(Float, m_side, 1.f, CVAR_GLOBALCONFIG | CVAR_ARCHIVE)
 
 void InputState::GetMouseDelta(ControlInfo * hidInput)
 {
-	hidInput->mouseturnx = g_mousePos.x * m_yaw * (1.f / 4.f);
-	hidInput->mouseturny = g_mousePos.y * m_pitch * (1.f / 5.f);
+	hidInput->mouseturnx = g_mousePos.x * m_yaw * (1.f / 18.f);
+	hidInput->mouseturny = g_mousePos.y * m_pitch * (1.f / 14.f);
 	hidInput->mousemovex = g_mousePos.x * m_side;
 	hidInput->mousemovey = g_mousePos.y * m_forward;
 
