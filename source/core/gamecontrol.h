e53d58bd4220e3aa044d606bdcba113934dd7783
Mitchell Richters <mjr4077au@gmail.com>
2020-10-08 14:29:27 +1100

    - gamecontrol: Clean up remainder of legacy angle code.

--- source/core/gamecontrol.h
+++ source/core/gamecontrol.h
@@ -74,14 +74,78 @@ struct PlayerHorizon
 	fixed_t target;
 	double adjustment;
 
-	void backup();
-	void restore();
-	void addadjustment(double value);
-	void resetadjustment();
-	void settarget(double value, bool backup = false);
-	void processhelpers(double const scaleAdjust);
-	fixedhoriz sum();
-	fixedhoriz interpolatedsum(double const smoothratio);
+	void backup()
+	{
+		ohoriz = horiz;
+		ohorizoff = horizoff;
+	}
+
+	void restore()
+	{
+		horiz = ohoriz;
+		horizoff = ohorizoff;
+	}
+
+	void addadjustment(double value)
+	{
+		if (!cl_syncinput)
+		{
+			adjustment += value;
+		}
+		else
+		{
+			horiz += q16horiz(FloatToFixed(value));
+		}
+	}
+
+	void resetadjustment()
+	{
+		adjustment = 0;
+	}
+
+	void settarget(double value, bool backup = false)
+	{
+		if (!cl_syncinput)
+		{
+			target = FloatToFixed(value);
+			if (target == 0) target += 1;
+		}
+		else
+		{
+			horiz = q16horiz(FloatToFixed(value));
+			if (backup) ohoriz = horiz;
+		}
+	}
+
+	void processhelpers(double const scaleAdjust)
+	{
+		if (target)
+		{
+			horiz += q16horiz(xs_CRoundToInt(scaleAdjust * (target - horiz.asq16())));
+
+			if (abs(horiz.asq16() - target) < FRACUNIT)
+			{
+				horiz = q16horiz(target);
+				target = 0;
+			}
+		}
+		else if (adjustment)
+		{
+			horiz += q16horiz(FloatToFixed(scaleAdjust * adjustment));
+		}
+	}
+
+	fixedhoriz sum()
+	{
+		return horiz + horizoff;
+	}
+
+	fixedhoriz interpolatedsum(double const smoothratio)
+	{
+		fixedhoriz prev = ohoriz + ohorizoff;
+		fixedhoriz curr = horiz + horizoff;
+		return q16horiz(prev.asq16() + mulscale16(curr.asq16() - prev.asq16(), smoothratio));
+	}
 };
 
 struct PlayerAngle
@@ -90,24 +154,90 @@ struct PlayerAngle
 	lookangle look_ang, olook_ang, rotscrnang, orotscrnang, spin;
 	double adjustment;
 
-	void backup();
-	void restore();
-	void addadjustment(double value);
-	void resetadjustment();
-	void settarget(double value, bool backup = false);
-	void processhelpers(double const scaleAdjust);
-	binangle sum();
-	binangle interpolatedsum(double const smoothratio);
-	lookangle interpolatedrotscrn(double const smoothratio);
+	void backup()
+	{
+		oang = ang;
+		olook_ang = look_ang;
+		orotscrnang = rotscrnang;
+	}
+
+	void restore()
+	{
+		ang = oang;
+		look_ang = olook_ang;
+		rotscrnang = orotscrnang;
+	}
+
+	void addadjustment(double value)
+	{
+		if (!cl_syncinput)
+		{
+			adjustment += value;
+		}
+		else
+		{
+			ang += bamang(xs_CRoundToUInt(value * BAMUNIT));
+		}
+	}
+
+	void resetadjustment()
+	{
+		adjustment = 0;
+	}
+
+	void settarget(double value, bool backup = false)
+	{
+		if (!cl_syncinput)
+		{
+			target = bamang(xs_CRoundToUInt(value * BAMUNIT));
+			if (target.asbam() == 0) target += bamang(1);
+		}
+		else
+		{
+			ang = bamang(xs_CRoundToUInt(value * BAMUNIT));
+			if (backup) oang = ang;
+		}
+	}
+
+	void processhelpers(double const scaleAdjust)
+	{
+		if (target.asbam())
+		{
+			ang = bamang(ang.asbam() + xs_CRoundToInt(scaleAdjust * (target - ang).asbam()));
+
+			if (ang.asbam() - target.asbam() < BAMUNIT)
+			{
+				ang = target;
+				target = bamang(0);
+			}
+		}
+		else if (adjustment)
+		{
+			ang += bamang(xs_CRoundToUInt(scaleAdjust * adjustment * BAMUNIT));
+		}
+	}
+
+	binangle sum()
+	{
+		return bamang(ang.asbam() + look_ang.asbam());
+	}
+
+	binangle interpolatedsum(double const smoothratio)
+	{
+		auto prev = oang.asbam() + olook_ang.asbam();
+		auto curr = ang.asbam() + look_ang.asbam();
+		return bamang(xs_CRoundToUInt(prev + fmulscale16(curr - prev, smoothratio)));
+	}
+
+	lookangle interpolatedrotscrn(double const smoothratio)
+	{
+		return bamlook(xs_CRoundToUInt(orotscrnang.asbam() + fmulscale16(rotscrnang.asbam() - orotscrnang.asbam(), smoothratio)));
+	}
 };
 
 void processMovement(InputPacket* currInput, InputPacket* inputBuffer, ControlInfo* const hidInput, double const scaleAdjust, int const drink_amt = 0, bool const allowstrafe = true, double const turnscale = 1);
 void sethorizon(fixedhoriz* horiz, float const horz, ESyncBits* actions, double const scaleAdjust);
 void applylook(PlayerAngle* angle, fixed_t const q16avel, ESyncBits* actions, double const scaleAdjust, bool const crouching);
-void applylook2(fixed_t* q16ang, fixed_t* q16look_ang, fixed_t* q16rotscrnang, fixed_t* spin, fixed_t const q16avel, ESyncBits* actions, double const scaleAdjust, bool const crouching);
-void playerAddAngle2(fixed_t* q16ang, double* helper, double adjustment);
-void playerSetAngle2(fixed_t* q16ang, fixed_t* helper, double adjustment);
-void playerProcessHelpers(fixed_t* q16ang, double* angAdjust, fixed_t* angTarget, double const scaleAdjust);
 
 struct UserConfig
 {
