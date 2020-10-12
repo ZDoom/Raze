a792f329ca3661b6b3cd264770ba1a719d58b8d0
Mitchell Richters <mjr4077au@gmail.com>
2020-10-12 16:28:05 +1100

    - binaryangle.h/PlayerAngle: Fix incorrect variable declaration on `dang` within `interpolatedsum()` method.
    * Didn't seem to be causing an issue but better it be correct.

--- source/core/gameinput.h
+++ source/core/gameinput.h
@@ -167,7 +167,7 @@ struct PlayerAngle
 	binangle interpolatedsum(double const smoothratio)
 	{
 		double const ratio = smoothratio / FRACUNIT;
-		int32_t const dang = UINT32_MAX / 2;
+		uint32_t const dang = UINT32_MAX >> 1;
 		int64_t const prev = oang.asbam() + olook_ang.asbam();
 		int64_t const curr = ang.asbam() + look_ang.asbam();
 		return bamang(prev + xs_CRoundToUInt(ratio * (((curr + dang - prev) & 0xFFFFFFFF) - dang)));
