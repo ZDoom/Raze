197a5f642afe9c020d223e3990c31a8947f6ed52
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-10-15 20:10:52 +0200

    - properly delete backing images in the texture manager.
    Although the images themselves are automatically taken down, any memory they allocate is not - and some of the special tile classes do need dynamic storage.

--- source/common/platform/win32/i_main.cpp
+++ source/common/platform/win32/i_main.cpp
@@ -1242,7 +1242,7 @@ int WINAPI wWinMain (HINSTANCE hInstance, HINSTANCE nothing, LPWSTR cmdline, int
 	_CrtSetDbgFlag (_CrtSetDbgFlag(0) | _CRTDBG_LEAK_CHECK_DF);
 
 	// Use this to break at a specific allocation number.
-	//_crtBreakAlloc = 227524;
+	//_crtBreakAlloc = 291757;
 #endif
 
 	int ret = DoMain (hInstance);
