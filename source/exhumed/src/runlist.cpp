8cbd67974eb32613d35438b324f8a5ff68a88e55
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-11-23 19:32:12 +0100

    - Exhumed: fixed typo in player taunt code.

--- source/exhumed/src/runlist.cpp
+++ source/exhumed/src/runlist.cpp
@@ -1637,7 +1637,7 @@ void runlist_DamageEnemy(int nSprite, int nSprite2, short nDamage)
     runlist_SendMessageToRunRec(nRun, (nSprite2 & 0xFFFF) | 0x80000, nDamage * 4);
 
     // is there now one less creature? (has one died)
-    if (nPreCreaturesKilled > nCreaturesKilled && nSprite2 > -1)
+    if (nPreCreaturesKilled < nCreaturesKilled && nSprite2 > -1)
     {
         if (sprite[nSprite2].statnum != 100) {
             return;
