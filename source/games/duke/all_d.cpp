0f3da7194a5ce3f49a5bd45204048b7d0b3a7c3e
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-01-04 11:45:50 +0100

    - actually compile the game code as larger units and integrate them in the main project.
    This eliminates the problematic subprojects for good.
    Compiling the games as larger units is necessary because otherwise compile times will break down on MSVC.

---
+++ source/games/duke/all_d.cpp
@@ -0,0 +1,15 @@
+#include "src/2d_d.cpp"
+#include "src/actors_d.cpp"
+#include "src/animatesprites_d.cpp"
+#include "src/flags_d.cpp"
+#include "src/hudweapon_d.cpp"
+#include "src/player_d.cpp"
+#include "src/player_w.cpp"
+#include "src/premap_d.cpp"
+#include "src/sbar_d.cpp"
+#include "src/sectors_d.cpp"
+#include "src/spawn_d.cpp"
+
+// These global files include names_d.h!
+#include "src/sounds.cpp"
+#include "src/gamevar.cpp"
