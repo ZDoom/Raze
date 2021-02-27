88d8e6421d5a0899f5f3ab8322bc3543580b9647
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-02-27 13:54:42 +0100

    - this is version 0.9.

--- source/core/version.h
+++ source/core/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "0.8.0 beta"
+#define VERSIONSTR "0.9.0 beta"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 0,8,0,0
-#define RC_PRODUCTVERSION 0,8,0,0
+#define RC_FILEVERSION 0,9,0,0
+#define RC_PRODUCTVERSION 0,9,0,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
@@ -53,7 +53,7 @@ const char *GetVersionString();
 #define VER_REVISION 0
 
 #define ENG_MAJOR 0
-#define ENG_MINOR 8
+#define ENG_MINOR 9
 #define ENG_REVISION 0
 
 // More stuff that needs to be different for derivatives.
