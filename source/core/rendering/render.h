0579368626bf20dcbd71deb16c2bb70a7560b68d
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-03-18 17:24:58 +0100

    - removed render.cpp, now that everything is in its proper place.

--- source/core/rendering/render.h
+++ source/core/rendering/render.h
@@ -2,5 +2,3 @@
 #include "build.h"
 
 void render_drawrooms(vec3_t& position, int sectnum, fixed_t q16angle, fixed_t q16horizon, float rollang, bool mirror, bool planemirror);
-void render_drawrooms_(vec3_t& position, int sectnum, fixed_t q16angle, fixed_t q16horizon, float rollang, float fov, bool mirror, bool planemirror);
-
