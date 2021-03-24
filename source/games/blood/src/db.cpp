13bbf0a4f98e5de38f4229d47bb03b2f287c7b85
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-03-24 23:55:10 +0100

    - sector portals in RRRA are working.

--- source/games/blood/src/db.cpp
+++ source/games/blood/src/db.cpp
@@ -512,6 +512,10 @@ void dbLoadMap(const char *pPath, int *pX, int *pY, int *pZ, short *pAngle, shor
     gModernMap = false;
     #endif
 
+    memset(sector, 0, sizeof(*sector) * MAXSECTORS);
+    memset(wall, 0, sizeof(*wall) * MAXWALLS);
+    memset(sprite, 0, sizeof(*sector) * MAXSPRITES);
+
 #ifdef USE_OPENGL
     Polymost::Polymost_prepare_loadboard();
 #endif
