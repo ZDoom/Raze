8e9a6e82d710c3b94a0f72c47c59a030d6c3d287
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-04-09 18:53:48 +0200

    - this is version 1.0.1.

--- source/core/version.h
+++ source/core/version.h
@@ -44,17 +44,17 @@ const char *GetVersionString();
 #define VERSIONSTR "1.0.0-pre"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 0,9,0,0
-#define RC_PRODUCTVERSION 0,9,0,0
+#define RC_FILEVERSION 1,0,1,0
+#define RC_PRODUCTVERSION 1,0,1,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
 #define VER_MINOR 5
 #define VER_REVISION 0
 
-#define ENG_MAJOR 0
-#define ENG_MINOR 8
-#define ENG_REVISION 0
+#define ENG_MAJOR 1
+#define ENG_MINOR 0
+#define ENG_REVISION 1
 
 // More stuff that needs to be different for derivatives.
 #define GAMENAME "Raze"
