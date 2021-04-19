e72d6e3df7089c311cf8cfd1a279ca7a401c841b
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-04-19 21:44:06 +0200

    - this is version 1.0.3

--- source/core/version.h
+++ source/core/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "1.0.0-pre"
+#define VERSIONSTR "1.0.3"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 0,9,0,0
-#define RC_PRODUCTVERSION 0,9,0,0
+#define RC_FILEVERSION 1.0.3,0
+#define RC_PRODUCTVERSION 1.0.3,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
