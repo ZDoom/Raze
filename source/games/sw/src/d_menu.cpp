df6a9823e393f3b191d0dfccfd02f9b71ad9ddf5
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-05-07 18:36:00 +0200

    - fixed incorrect license in sw/d_menu.cpp.
    Should be GPL 2+, not GPL 2.

--- source/games/sw/src/d_menu.cpp
+++ source/games/sw/src/d_menu.cpp
@@ -1,11 +1,11 @@
 //-------------------------------------------------------------------------
 /*
-Copyright (C) 2016 EDuke32 developers and contributors
 Copyright (C) 2019 Christoph Oelckers
 
 This is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License version 2
-as published by the Free Software Foundation.
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
