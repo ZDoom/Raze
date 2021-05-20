d4eb54d6866b806929a2785a635eae08f098d2fb
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-05-20 12:36:35 +0200

    - render free standing wall sprites without depth bias.
    Otherwise they can end up badly ordered with walls.

--- source/core/rendering/scene/hw_drawstructs.h
+++ source/core/rendering/scene/hw_drawstructs.h
@@ -133,6 +133,7 @@ public:
 	{
 		RWF_BLANK = 0,
 		RWF_TEXTURED = 1,	// actually not being used anymore because with buffers it's even less efficient not writing the texture coordinates - but leave it here
+		RWF_TRANS = 2,
 		RWF_NORENDER = 8,
 	};
 
@@ -158,6 +159,7 @@ public:
 	
 	float ViewDistance;
 	float visibility;
+	int walldist;
 	short shade, palette;
 
 	PalEntry fade;
