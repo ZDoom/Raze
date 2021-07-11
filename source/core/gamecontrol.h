ffba10a7c5f79d0b4051d3014e28f74384230e5c
Mitchell Richters <mjr4077au@gmail.com>
2021-07-11 12:08:12 +1000

    - SW: Set default skill level to match original release and what the manual says is default.

--- source/core/gamecontrol.h
+++ source/core/gamecontrol.h
@@ -197,6 +197,11 @@ inline bool isBlood()
 	return g_gameType & GAMEFLAG_BLOOD;
 }
 
+inline bool isSWALL()
+{
+	return g_gameType & (GAMEFLAG_SW | GAMEFLAG_SWWANTON | GAMEFLAG_SWTWINDRAG);
+}
+
 TArray<GrpEntry> GrpScan();
 void S_PauseSound(bool notmusic, bool notsfx);
 void S_ResumeSound(bool notsfx);
--- source/core/menu/razemenu.cpp
+++ source/core/menu/razemenu.cpp
@@ -465,7 +465,7 @@ static void BuildEpisodeMenu()
 		{
 			ld->mItems.Pop(popped);
 		}
-		if (isBlood()) gDefaultSkill = 2;
+		if (isBlood() || isSWALL()) gDefaultSkill = 2;
 		ld->mSelectedItem = gDefaultSkill + ld->mItems.Size(); // account for pre-added items
 		double y = ld->mYpos;
 
