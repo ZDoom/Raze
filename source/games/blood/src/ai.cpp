f370c0f22f498227f3beda5c7813c8778854fe2b
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-09-10 23:36:18 +0200

    - fixed bad null check in aiProcessDudes.

--- source/games/blood/src/ai.cpp
+++ source/games/blood/src/ai.cpp
@@ -1655,11 +1655,14 @@ void aiProcessDudes(void)
 
 		pXSprite->stateTimer = ClipLow(pXSprite->stateTimer - 4, 0);
 
-		if (pXSprite->aiState && pXSprite->aiState->moveFunc)
-			pXSprite->aiState->moveFunc(actor);
+		if (pXSprite->aiState)
+		{
+			if (pXSprite->aiState->moveFunc)
+				pXSprite->aiState->moveFunc(actor);
 
-		if (pXSprite->aiState->thinkFunc && (gFrameCount & 3) == (pSprite->index & 3)) // ouch, ouch! :(
-			pXSprite->aiState->thinkFunc(actor);
+			if (pXSprite->aiState->thinkFunc && (gFrameCount & 3) == (actor->GetIndex() & 3)) // ouch, ouch! :(
+				pXSprite->aiState->thinkFunc(actor);
+		}
 
 		switch (pSprite->type) {
 #ifdef NOONE_EXTENSIONS
