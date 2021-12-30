f679b61a30c66c9a14fa927c5f94242c50b8491e
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-12-30 22:56:33 +0100

    - fixed issue with wall sprite angle check.
    The resulting value had no sign, so 'abs' did not work. It now uses some bit shifting magic to get a proper sign.

--- source/core/rendering/scene/hw_walls.cpp
+++ source/core/rendering/scene/hw_walls.cpp
@@ -61,7 +61,7 @@ static walltype* IsOnWall(tspritetype* tspr, int height, DVector2& outpos)
 		// Intentionally include two sided walls. Even on them the sprite should be projected onto the wall for better results.
 		auto d = wal.delta();
 		int walang = getangle(d.X, d.Y);
-		int deltaang = abs(walang - tspr->ang) & 2047;
+		int deltaang = abs((((walang - tspr->ang) & 2047) << 21) >> 21);
 		const int maxangdelta = 1;
 
 		// angle of the sprite must either be the wall's normal or the negative wall's normal to be aligned.
