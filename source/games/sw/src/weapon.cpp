86a7f7ca2c94437e2d7dbed45c0caa57f26b6415
Mitchell Richters <mjr4077au@gmail.com>
2022-10-11 16:59:51 +1100

    - SW: Pitch-adjusted velocity for `InitTankShell()`.

--- source/games/sw/src/weapon.cpp
+++ source/games/sw/src/weapon.cpp
@@ -15697,7 +15697,7 @@ int InitTankShell(DSWActor* actor, PLAYER* pp)
     actorNew->spr.cstat |= (CSTAT_SPRITE_YCENTER);
     actorNew->spr.cstat |= (CSTAT_SPRITE_INVISIBLE);
 
-    actorNew->vel.Z = pp->horizon.horiz.Tan() * actorNew->vel.X;
+    setFreeAimVelocity(actorNew->vel.X, actorNew->vel.Z, pp->horizon.horiz, actorNew->vel.X);
 
     WeaponAutoAim(actor, actorNew, DAngle22_5 / 2, false);
     // a bit of randomness
