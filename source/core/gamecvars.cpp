16b48bbd4d5212eb20380e62788a15df8a37a901
Mitchell Richters <mjr4077au@gmail.com>
2022-10-25 11:47:00 +1100

    - Default display of map name in SW to be off by default.

--- source/core/gamecvars.cpp
+++ source/core/gamecvars.cpp
@@ -180,7 +180,7 @@ CUSTOM_CVARD(Float, hud_statscale, 0.5, CVAR_ARCHIVE, "change the scale of the s
 
 
 CVARD(Bool, hud_stats, false, CVAR_ARCHIVE|CVAR_GLOBALCONFIG, "enable/disable level statistics display")
-CVARD(Bool, hud_showmapname, true, CVAR_ARCHIVE|CVAR_GLOBALCONFIG, "enable/disable map name display on load")
+CVARD(Bool, hud_showmapname, true, CVAR_ARCHIVE, "enable/disable map name display on load")
 CVARD(Bool, hud_position, false, CVAR_ARCHIVE, "aligns the status bar to the bottom/top")
 CVARD(Bool, hud_bgstretch, false, CVAR_ARCHIVE, "enable/disable background image stretching in wide resolutions")
 CVARD(Bool, hud_messages, true, CVAR_ARCHIVE, "enable/disable showing messages")
