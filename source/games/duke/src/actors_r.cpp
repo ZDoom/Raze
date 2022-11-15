202137ed69aa6c96c2ae193be250025d97dcfafc
Mitchell Richters <mjr4077au@gmail.com>
2022-11-15 21:25:13 +1100

    - Duke: Fix interpolation issues with player's XY coordinates while drawing using the sprite.

--- source/games/duke/src/actors_r.cpp
+++ source/games/duke/src/actors_r.cpp
@@ -3307,7 +3307,7 @@ void move_r(DDukeActor *actor, int pnum, int xvel)
 	{
 		if ((badguy(actor) && actor->spr.extra <= 0) || (actor->opos.X != actor->spr.pos.X) || (actor->opos.Y != actor->spr.pos.Y))
 		{
-			actor->backupvec2();
+			if (actor->spr.picnum != APLAYER) actor->backupvec2();
 			SetActor(actor, actor->spr.pos);
 		}
 		if (badguy(actor) && actor->spr.extra <= 0)
