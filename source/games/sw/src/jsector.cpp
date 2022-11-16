7757288d33845edbbae56a6bdf2677290d83fd92
Mitchell Richters <mjr4077au@gmail.com>
2022-11-16 13:25:19 +1100

    - SW: Tidy up one vector equality test.

--- source/games/sw/src/jsector.cpp
+++ source/games/sw/src/jsector.cpp
@@ -598,7 +598,7 @@ void JS_DrawCameras(PLAYER* pp, const DVector3& campos, double smoothratio)
 
                     // If player is dead still then update at MoveSkip4
                     // rate.
-                    if (pp->PlayerNowPosition.X == pp->PlayerPrevPosition.X && pp->PlayerNowPosition.Y == pp->PlayerPrevPosition.Y && pp->PlayerNowPosition.Z == pp->PlayerPrevPosition.Z)
+                    if (pp->PlayerNowPosition == pp->PlayerPrevPosition)
                         DoCam = true;
 
 
