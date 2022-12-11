940aff64cef1e2f7a4469955252b9ee093078889
Christoph Oelckers <coelckers@users.noreply.github.com>
2022-12-11 18:31:07 +0100

    - we're past 1.6 now.

--- source/core/version.h
+++ source/core/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "1.6pre"
+#define VERSIONSTR "1.7pre"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 1,5,9999,0
-#define RC_PRODUCTVERSION 1,5,9999,0
+#define RC_FILEVERSION 1,6,9999,0
+#define RC_PRODUCTVERSION 1,6,9999,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
@@ -53,7 +53,7 @@ const char *GetVersionString();
 #define VER_REVISION 0
 
 #define ENG_MAJOR 1
-#define ENG_MINOR 5
+#define ENG_MINOR 6
 #define ENG_REVISION 0
 
 // More stuff that needs to be different for derivatives.
