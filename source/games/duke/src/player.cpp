a2e9191c09817510d8b3134a98675d74c0571c13
Mitchell Richters <mjr4077au@gmail.com>
2023-03-17 12:29:37 +1100

    - Duke: Remove `player_struct::resurrected` flag I added in 2021 and fix issue properly.
    * The check for `player_struct::dead_flag` in `FinalizeInput()` was overzealous and is not checked in the original code in this instance.

--- source/games/duke/src/player.cpp
+++ source/games/duke/src/player.cpp
@@ -739,7 +739,7 @@ void playerJump(int snum, int fz, int cz)
 
 void player_struct::apply_seasick(double factor)
 {
-	if (isRRRA() && SeaSick && (dead_flag == 0 || (dead_flag && resurrected)))
+	if (isRRRA() && SeaSick && (dead_flag == 0))
 	{
 		if (SeaSick < 250)
 		{
