5f8cd74ba50536b0c1f61b29e4261f40de481eeb
Christoph Oelckers <coelckers@users.noreply.github.com>
2023-09-30 08:53:46 +0200

    added missing license to Duke's vmexports.cpp

--- source/games/duke/src/vmexports.cpp
+++ source/games/duke/src/vmexports.cpp
@@ -1,3 +1,23 @@
+//-------------------------------------------------------------------------
+/*
+Copyright (C) 2020-2023 - Christoph Oelckers
+
+This file is part of Raze
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+*/
+//-------------------------------------------------------------------------
+
 #include "vm.h"
 #include "ns.h"
 #include "buildtiles.h"
