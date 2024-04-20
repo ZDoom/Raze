3fe0c2e698cb7ea70b058295eb1e8399e4e03518
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2024-04-20 00:13:39 -0400

    - add in a common code stub

---
+++ source/core/vmstubs.cpp
@@ -0,0 +1,8 @@
+#include "common/engine/palettecontainer.h"
+#include "name.h"
+#include "dobject.h"
+
+bool ShouldAllowGameSpecificVirtual(FName name, unsigned index, PType* arg, PType* varg)
+{
+    return false;
+}
