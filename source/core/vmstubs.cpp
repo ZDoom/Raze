df3b193250ddd557da9b9e7e3e3010b20b4470f7
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2024-04-20 19:14:04 -0400

    - create stubs required for https://github.com/ZDoom/gzdoom/pull/2514

--- source/core/vmstubs.cpp
+++ source/core/vmstubs.cpp
@@ -1,8 +1,22 @@
-#include "common/engine/palettecontainer.h"
 #include "name.h"
 #include "dobject.h"
 
 bool ShouldAllowGameSpecificVirtual(FName name, unsigned index, PType* arg, PType* varg)
 {
-    return false;
+	return false;
+}
+
+void DObject::EnableNetworking(const bool enable)
+{
+	return;
+}
+
+void DObject::RemoveFromNetwork(void)
+{
+	return;
+}
+
+void DObject::ClearNetworkID()
+{
+	return;
 }
